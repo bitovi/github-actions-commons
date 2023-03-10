1477793f376b759bb9a720325e190b9a35c432f3
LeoDiazL <leo@bitovi.com>
2023-03-10 15:55:36 -0300

    Changing file check

--- operations/_scripts/deploy/export_vars.sh
+++ operations/_scripts/deploy/export_vars.sh
@@ -4,7 +4,7 @@
 BO_OUT="$GITHUB_ACTION_PATH/operations/bo-out.env"
 
 echo "Check for $BO_OUT"
-if [ -f $BO_OUT ]; then
+if [ -s $BO_OUT ]; then
   echo "Outputting bo-out.env to GITHUB_OUTPUT"
   cat $BO_OUT >> $GITHUB_OUTPUT
 else
