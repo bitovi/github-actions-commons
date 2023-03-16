d5dc5cee3becbf524883f02a0c6169354f7debae
LeoDiazL <116020815+LeoDiazL@users.noreply.github.com>
2023-03-16 15:57:59 -0300

    Fixing templates

--- operations/deployment/generators/modules/aws/exports/exports.tf
+++ operations/deployment/generators/modules/aws/exports/exports.tf
@@ -1,9 +1,9 @@
 resource "local_file" "aws_dotenv_secretmanager" {
     filename = format("%s/%s", abspath(path.root), "aws_dotenv_secretmanager.tf")
-    content  = file("${path.module}/aws/aws_dotenv_secretmanager.tmpl")
+    content = file(format("%s/%s", abspath(path.module), "aws_dotenv_secretmanager.tmpl"))
 }
 
 resource "local_file" "aws_dotenv_tf" {
     filename = format("%s/%s", abspath(path.root), "aws_dotenv_tf.tf")
-    content  = file("${path.module}/aws/aws_dotenv_tf.tmpl")
+    content = file(format("%s/%s", abspath(path.module), "aws_dotenv_tf.tmpl"))
 }
