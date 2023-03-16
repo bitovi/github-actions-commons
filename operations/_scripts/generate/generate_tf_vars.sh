fa1fbe38fcf2bcda334a1e98c082cdbe63dead6b
LeoDiazL <116020815+LeoDiazL@users.noreply.github.com>
2023-03-16 11:33:46 -0300

    Adding support for EC2 port list

--- operations/_scripts/generate/generate_tf_vars.sh
+++ operations/_scripts/generate/generate_tf_vars.sh
@@ -102,6 +102,8 @@ aws_ec2_ami_id=$(generate_var aws_ec2_ami_id $AWS_EC2_AMI_ID)
 aws_ec2_instance_type=$(generate_var aws_ec2_instance_type $AWS_EC2_INSTANCE_TYPE)
 aws_ec2_create_keypair_sm=$(generate_var aws_ec2_create_keypair_sm $AWS_EC2_CREATE_KEYPAIR_SM)
 aws_ec2_instance_public_ip=$(generate_var aws_ec2_instance_public_ip $AWS_EC2_INSTANCE_PUBLIC_IP)
+aws_ec2_port_list=$(generate_var aws_ec2_port_list $AWS_EC2_PORT_LIST)
+
 
 
 #-- AWS Route53 and certs --#
--- operations/deployment/terraform/variables.tf
+++ operations/deployment/terraform/variables.tf
@@ -56,6 +56,11 @@ variable "aws_ec2_instance_public_ip" {
   description = "Attach public IP to the EC2 instance"
 }
 
+variable "aws_ec2_port_list" {
+  type = list(number)
+  default = []
+}
+
 # AWS Route53 Domains abd Certificates
 
 variable "aws_r53_domain_name" {
@@ -283,11 +288,6 @@ variable "availability_zone" {
 }
 
 # EC2 
-
-#variable "aws_ec2_port_list" {
-#  type = list(number)
-#  default = []
-#}
 
 
 variable "security_group_name" {
