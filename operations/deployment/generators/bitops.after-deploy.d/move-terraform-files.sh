f2180cc8332fe747fb95d7d45adbd11cf6c155b7
LeoDiazL <leo@bitovi.com>
2023-03-21 14:39:05 -0300

    Typo

--- operations/deployment/generators/bitops.after-deploy.d/move-terraform-files.sh
+++ operations/deployment/generators/bitops.after-deploy.d/move-terraform-files.sh
@@ -8,6 +8,7 @@ echo "BitOps Ansible before script: Merge Terraform Enviornment Variables..."
 
 ls -lah "${BITOPS_ENVROOT}/generators"
 rm "${BITOPS_ENVROOT}/generators/generator.tf"
-rm "${BITOPS_ENVROOT}/generators/variables.tf*"
+rm "${BITOPS_ENVROOT}/generators/variables.tf"
+rm "${BITOPS_ENVROOT}/generators/variables.tfvars"
 mv ${BITOPS_ENVROOT}/generators/*.tf "${BITOPS_ENVROOT}/terraform/."
 ls -lah "${BITOPS_ENVROOT}/terraform"
