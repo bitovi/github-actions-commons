286c557deef5fb6237fbb4a680c863610c617a15
LeoDiazL <leo@bitovi.com>
2023-03-24 13:30:43 -0300

    typos

--- operations/deployment/generators/modules/aws/elb/elb.tf
+++ operations/deployment/generators/modules/aws/elb/elb.tf
@@ -1,7 +1,7 @@
 resource "local_file" "aws_elb" {
     filename = format("%s/%s", abspath(path.root), "aws_elb.tf")
     content  = templatefile(format("%s/%s", abspath(path.module), "aws_elb.tmpl"), {
-      cert_string = var.aws_r53_enable_cert ? "\"${local.selected_arn}\"" : ""
+      cert_string = var.aws_r53_enable_cert ? "\"local.selected_arn\"" : ""
     })
 }
 
