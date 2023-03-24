ab7937ff3e398906c2637bbe01ba516d09d2ebd3
LeoDiazL <leo@bitovi.com>
2023-03-24 12:04:16 -0300

    Fixing empty var

--- operations/deployment/generators/modules/aws/elb_no_cert/elb.tf
+++
@@ -1,4 +0,0 @@
-resource "local_file" "aws_elb_no_cert" {
-    filename = format("%s/%s", abspath(path.root), "aws_elb_no_cert.tf")
-    content = file(format("%s/%s", abspath(path.module), "aws_elb_no_cert.tmpl"))
-}
--- operations/deployment/generators/modules/aws/elb_with_cert/elb.tf
+++
@@ -1,4 +0,0 @@
-resource "local_file" "aws_elb_with_cert" {
-    filename = format("%s/%s", abspath(path.root), "aws_elb_with_cert.tf")
-    content = file(format("%s/%s", abspath(path.module), "aws_elb_with_cert.tmpl"))
-}
