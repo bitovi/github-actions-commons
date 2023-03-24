ec1ce8f65b230260ba9d226a5100b5c785a602cd
LeoDiazL <leo@bitovi.com>
2023-03-24 11:52:12 -0300

    Fix

--- operations/deployment/generators/modules/aws/elb/elb.tf
+++ operations/deployment/generators/modules/aws/elb/elb.tf
@@ -1,16 +1,12 @@
 resource "local_file" "aws_elb" {
     filename = format("%s/%s", abspath(path.root), "aws_elb.tf")
     content  = templatefile(format("%s/%s", abspath(path.module), "aws_elb.tmpl"), {
-      cert_string = local.cert_string
-  })
+      cert_string = var.aws_r53_enable_cert ? "local.selected_arn" : ""
+    })
 }
 
 # Root module inputs
 variable "aws_r53_enable_cert" {
   type        = bool
   default     = false
-}
-# Local definitions for template
-locals {
-  cert_string = var.aws_r53_enable_cert ? "local.selected_arn" : ""
 }
