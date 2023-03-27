516c7c8b6aa25ac56e445c8bee34fd8e293818cd
LeoDiazL <leo@bitovi.com>
2023-03-27 11:51:24 -0300

    Fix per var type

--- operations/deployment/generators/modules/aws/exports/exports.tf
+++ operations/deployment/generators/modules/aws/exports/exports.tf
@@ -1,5 +1,5 @@
 resource "local_file" "aws_dotenv_secretmanager" {
-    count = var.env_aws_secret ? 1 : 0
+    count = var.env_aws_secret != "" ? 1 : 0
     filename = format("%s/%s", abspath(path.root), "aws_dotenv_secretmanager.tf")
     content = file(format("%s/%s", abspath(path.module), "aws_dotenv_secretmanager.tmpl"))
 }
