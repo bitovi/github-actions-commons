e0bcc89d0ce779dadf2e0b9dd4a89174af7c8858
LeoDiazL <leo@bitovi.com>
2023-03-27 11:56:07 -0300

    Typos

--- operations/deployment/generators/modules/aws/exports/exports.tf
+++ operations/deployment/generators/modules/aws/exports/exports.tf
@@ -8,8 +8,8 @@ resource "local_file" "aws_dotenv_tf" {
     filename = format("%s/%s", abspath(path.root), "aws_dotenv_tf.tf")
     content  = templatefile(format("%s/%s", abspath(path.module), "aws_dotenv_tf.tmpl"), {
       aws_instance_url_string = var.aws_ec2_instance_create ? "$${aws_instance.server.public_dns}" : ""
-      host_dir_string         = var.aws_efs_create || var.aws_efs_create_ha ? "$${aws_efs_ec2_mount_point}" : ""
-      target_dir_string       = var.aws_efs_create || var.aws_efs_create_ha ? "$${docker_efs_mount_target}" : ""
+      host_dir_string         = var.aws_efs_create || var.aws_efs_create_ha ? "$${var.aws_efs_ec2_mount_point}" : ""
+      target_dir_string       = var.aws_efs_create || var.aws_efs_create_ha ? "$${var.docker_efs_mount_target}" : ""
     })
 }
 
