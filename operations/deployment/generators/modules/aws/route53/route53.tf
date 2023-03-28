6a09e2784b43a2e6808d9d3bc1b655d8baddf934
LeoDiazL <leo@bitovi.com>
2023-03-28 12:11:31 -0300

    typos

--- operations/deployment/generators/modules/aws/route53/route53.tf
+++ operations/deployment/generators/modules/aws/route53/route53.tf
@@ -2,7 +2,7 @@ resource "local_file" "aws_route53" {
     filename = format("%s/%s", abspath(path.root), "aws_route53.tf")
     content  = templatefile(format("%s/%s", abspath(path.module), "aws_route53.tmpl"), {
       elb_resource_string = var.aws_r53_enable_cert ? "vm_lb_ssl" : "vm_lb"
-      r53_protocol_string = var.aws_r53_enable_cert ? "local.selected_arn != \"" ? "https://\"" : "\"http://\"" : "\"http://\""
+      r53_protocol_string = var.aws_r53_enable_cert ? "local.selected_arn != \"\" ? \"https://\" : \"http://\"" : "\"http://\""
     })
 }
 
