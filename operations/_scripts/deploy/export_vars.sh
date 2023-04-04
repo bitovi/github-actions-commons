9213ffd69cac0bb8d832e3ff8ffe2a38f328d257
LeoDiazL <leo@bitovi.com>
2023-04-04 09:19:14 -0300

    Adding a nice message when destroying and not exporting vars

--- operations/_scripts/deploy/export_vars.sh
+++ operations/_scripts/deploy/export_vars.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # Export variables to GHA
 
-BO_OUT="$GITHUB_ACTION_PATH/operations/bo-out.env"
-
-echo "Check for $BO_OUT"
-if [ -s $BO_OUT ]; then
-  echo "Outputting bo-out.env to GITHUB_OUTPUT"
-  cat $BO_OUT >> $GITHUB_OUTPUT
+if [ "$TF_STACK_DESTROY" != "true" ]; then
+  BO_OUT="$GITHUB_ACTION_PATH/operations/bo-out.env"
+  
+  echo "Check for $BO_OUT"
+  if [ -s $BO_OUT ]; then
+    echo "Outputting bo-out.env to GITHUB_OUTPUT"
+    cat $BO_OUT >> $GITHUB_OUTPUT
+  else
+    echo "BO_OUT is not a file or it's empty"
+  fi
 else
-  echo "BO_OUT is not a file or it's empty"
+  echo "Destrit execution. No variables to be exported."
 fi
