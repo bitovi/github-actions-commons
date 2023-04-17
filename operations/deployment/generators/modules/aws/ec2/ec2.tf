3da47bd96647295d45afa129d7ec9224c3cd78e3
LeoDiazL <leo@bitovi.com>
2023-04-17 16:32:48 -0300

    Removing unnecesary comments

--- operations/deployment/generators/modules/aws/ec2/ec2.tf
+++ operations/deployment/generators/modules/aws/ec2/ec2.tf
@@ -8,11 +8,6 @@ resource "local_file" "aws_ec2_security_group" {
     content = file(format("%s/%s", abspath(path.module), "aws_ec2_security_group.tmpl"))
 }
 
-#resource "local_file" "aws_ec2" {
-#    filename = format("%s/%s", abspath(path.root), "aws_ec2.tf")
-#    content = file(format("%s/%s", abspath(path.module), "aws_ec2.tmpl"))
-#}
-
 resource "local_file" "aws_ec2" {
     filename = format("%s/%s", abspath(path.root), "aws_ec2.tf")
     content  = templatefile(format("%s/%s", abspath(path.module), "aws_ec2.tmpl"), {
