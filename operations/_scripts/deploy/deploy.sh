a36a1d3938c537cfb73c1763ad9bd7d3bc9f209c
LeoDiazL <leo@bitovi.com>
2023-04-17 11:50:56 -0300

    Missing --env-file

--- operations/_scripts/deploy/deploy.sh
+++ operations/_scripts/deploy/deploy.sh
@@ -45,7 +45,7 @@ mkdir -p "${GITHUB_ACTION_PATH}/operations/deployment/env-files"
 echo "$ENV_GHV" > "${GITHUB_ACTION_PATH}/operations/deployment/env-files/ghv.env"
 echo "$ENV_GHS" > "${GITHUB_ACTION_PATH}/operations/deployment/env-files/ghs.env"
 if [ -s "$GITHUB_WORKSPACE/$ENV_REPO" ] && [ -n "$ENV_REPO" ]; then
-  cp "$GITHUB_WORKSPACE/$ENV_REPO" "${GITHUB_ACTION_PATH}/operations/deployment/env-files/repo.env"
+  cp "$GITHUB_WORKSPACE/$ENV_REPO" "--env-file ${GITHUB_ACTION_PATH}/operations/deployment/env-files/repo.env"
 fi
 
 TERRAFORM_COMMAND=""
