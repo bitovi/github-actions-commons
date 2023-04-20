15149f1786b267cb990ca8ff385524fafda567a0
LeoDiazL <leo@bitovi.com>
2023-04-20 11:27:50 -0300

    Fix conditional

--- operations/_scripts/generate/generate_vars_terraform.sh
+++ operations/_scripts/generate/generate_vars_terraform.sh
@@ -95,7 +95,7 @@ fi
 echo "AWS Postgres subnets: $aws_postgres_subnets"
 
 aws_postgres_database_final_snapshot=
-if [ -n "$AWS_POSTGRES_DATABASE_FINAL_SNAPSHOT" ];then
+if ! [ -n "$AWS_POSTGRES_DATABASE_FINAL_SNAPSHOT" ];then
   if [[ $(alpha_only "$2") == "true" ]]; then
     aws_postgres_database_final_snapshot="aws_postgres_database_final_snapshot = \"${GITHUB_IDENTIFIER}\""
   else
