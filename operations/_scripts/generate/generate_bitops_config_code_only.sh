c5a8a84b406e7980bdb98b29029feb2d67d9d9ce
LeoDiazL <leo@bitovi.com>
2023-04-27 17:35:46 -0300

    Fixing new inputs

--- operations/_scripts/generate/generate_bitops_config_code_only.sh
+++ operations/_scripts/generate/generate_bitops_config_code_only.sh
@@ -8,35 +8,6 @@ function alpha_only() {
   echo "$1" | tr -cd '[:alpha:]' | tr '[:upper:]' '[:lower:]'
 }
 
-CONFIG_STACK_ACTION="apply"
-if [ "$TF_STACK_DESTROY" == "true" ]; then
-  CONFIG_STACK_ACTION="destroy"
-  ANSIBLE_SKIP=true
-fi
-
-targets_attribute="targets:"
-if [ -n "$TF_TARGETS" ]; then
-  # Iterate over the provided comma-delimited string
-  for item in $(echo $TF_TARGETS | sed "s/,/ /g"); do
-    # Add the item to the YAML list
-  targets="$targets 
-      - $item"
-  done
-fi
-### random_integer.az_select needs to be created before the "full stack" to avoid a potential state dependency locks
-##targets="$targets
-##    - random_integer.az_select"
-##targets_attribute="$targets_attribute $targets"
-##
-### Terraform Bitops Config
-##echo -en "
-##terraform:
-##  cli:
-##    stack-action: $CONFIG_STACK_ACTION
-##    $targets_attribute
-##  options: {}
-##" > $GITHUB_ACTION_PATH/operations/deployment/terraform/bitops.config.yaml
-
 # Destination file
 BITOPS_CONFIG_FINAL="${GITHUB_ACTION_PATH}/operations/generated_code/bitops.config.yaml"
 BITOPS_CONFIG_TEMP="/tmp/bitops.config.yaml"
