4a212b3da3d847b2602f5f87559460832e4f2b8c
LeoDiazL <leo@bitovi.com>
2023-04-28 16:44:40 -0300

    Fix2

--- operations/_scripts/generate/generate_bitops_incoming.sh
+++ operations/_scripts/generate/generate_bitops_incoming.sh
@@ -72,10 +72,10 @@ if [ -n "$GH_ACTION_REPO" ]; then
     merge_tf_vars "$GH_ACTION_INPUT_TERRAFORM_PATH"
 
     cd "$GH_ACTION_INPUT_TERRAFORM_PATH"
-    for file in $(find . -maxdepth 1 -type f ); do
+    for file in "$(find . -maxdepth 1 -type f )"; do
       mv "$file" "$GITHUB_ACTION_PATH/operations/deployment/terraform/action_${file##*/}"
     done
-    for folder in $(find . -maxdepth 1 -type d -not -name "."); do 
+    for folder in "$(find . -maxdepth 1 -type d -not -name ".")"; do 
       mv "$folder" "${GITHUB_ACTION_PATH}/operations/deployment/terraform/."
     done
     cd -
@@ -126,10 +126,10 @@ if [ -n "$GH_DEPLOYMENT_INPUT_TERRAFORM" ]; then
   merge_tf_vars "$GH_DEPLOYMENT_INPUT_TERRAFORM_PATH"
 
   cd "$GH_DEPLOYMENT_INPUT_TERRAFORM_PATH"
-  for file in $(find . -maxdepth 1 -type f ); do
-    mv "${file}" "$GITHUB_ACTION_PATH/operations/deployment/terraform/deployment_${file##*/}"
+  for file in "$(find . -maxdepth 1 -type f )"; do
+    mv "$file" "$GITHUB_ACTION_PATH/operations/deployment/terraform/deployment_${file##*/}"
   done
-  for folder in $(find . -maxdepth 1 -type d -not -name "."); do 
+  for folder in "$(find . -maxdepth 1 -type d -not -name ".")"; do 
     mv "$folder" "${GITHUB_ACTION_PATH}/operations/deployment/terraform/."
   done
   cd -
