44bc6424d050867f82adfbd844a1dae5d27c67f8
LeoDiazL <leo@bitovi.com>
2023-05-01 18:22:53 -0300

    Debug

--- operations/deployment/terraform/bitops.after-deploy.d/generate-outputs.sh
+++ operations/deployment/terraform/bitops.after-deploy.d/generate-outputs.sh
@@ -18,12 +18,16 @@ echo "In afterhook - generate-outputs.sh"
 if [ "$BITOPS_TERRAFORM_COMMAND" != "destroy" ]; then
     # The sed command will make each variable be in it's line, and in case a list is present, will transform it into a line
     terraform output | sed -e ':a;/["\)]$/!N;s/\n//;ta' -e 's/ *= */=/g;s/[" ]//g;s/,\([]]\)/\1/g'  > /opt/bitops_deployment/bo-out.env
-    echo "Catting bo-out.env"
+    # Generating ec2 terraform .env
+    if ! [ -n $BITOPS_EC2_PUBLIC_URL ]; then
 echo -en "
 #### EC2 values  deployments:
 AWS_INSTANCE_URL="$BITOPS_EC2_PUBLIC_URL"
 
 " > $BITOPS_ENVROOT/terraform/ec2.env
+    echo "Cat ec2.env"
+    cat  $BITOPS_ENVROOT/terraform/ec2.env
+    fi
 
 fi
 echo "end terraform output for bo-out"
