192ad0e6ce88f36e79e92a53b70b6d29e88d2d46
LeoDiazL <leo@bitovi.com>
2023-05-01 14:18:49 -0300

    Changing EC2 DNS-IP to Ansible

--- operations/deployment/terraform/bitops.after-deploy.d/generate-outputs.sh
+++ operations/deployment/terraform/bitops.after-deploy.d/generate-outputs.sh
@@ -19,5 +19,15 @@ if [ "$BITOPS_TERRAFORM_COMMAND" != "destroy" ]; then
     # The sed command will make each variable be in it's line, and in case a list is present, will transform it into a line
     terraform output | sed -e ':a;/["\)]$/!N;s/\n//;ta' -e 's/ *= */=/g;s/[" ,]//g;s/,\([]]\)/\1/g'  > /opt/bitops_deployment/bo-out.env
     cat /opt/bitops_deployment/bo-out.env
+    export BITOPS_EC2_PUBLIC_IP=$(cat /opt/bitops_deployment/bo-out.env| grep instance_public_ip | awk -F"\"" '{print $2}')
+    export BITOPS_EC2_PUBLIC_URL=$(cat /opt/bitops_deployment/bo-out.env| grep instance_public_dns | awk -F"\"" '{print $2}')
+    echo "BITOPS_EC2_PUBLIC_URL $BITOPS_EC2_PUBLIC_URL"
+    echo "BITOPS_EC2_PUBLIC_IP $BITOPS_EC2_PUBLIC_IP"
+echo -en "
+#### EC2 values  deployments:
+AWS_INSTANCE_URL="$BITOPS_EC2_PUBLIC_URL"
+
+" > $BITOPS_ENVROOT/terraform/ec2.env
+
 fi
 echo "end terraform output for bo-out"
