50d6f5412c795299bf2bd400fef6e827d9f9e74a
LeoDiazL <leo@bitovi.com>
2023-05-02 09:51:48 -0300

    Code cleanup

--- operations/deployment/ansible/clone_repo/bitops.before-deploy.d/export_vars.sh
+++ operations/deployment/ansible/clone_repo/bitops.before-deploy.d/export_vars.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Export variables to Ansible
 
-export BITOPS_EC2_PUBLIC_IP="$(cat /opt/bitops_deployment/bo-out.env| grep instance_public_ip | awk -F"=" '{print $2}')"
-export BITOPS_EC2_PUBLIC_URL="$(cat /opt/bitops_deployment/bo-out.env| grep instance_public_dns | awk -F"=" '{print $2}')"
+BITOPS_EC2_PUBLIC_IP="$(cat /opt/bitops_deployment/bo-out.env| grep instance_public_ip | awk -F"=" '{print $2}')"
 sed -i "s/BITOPS_EC2_PUBLIC_IP/$(echo $BITOPS_EC2_PUBLIC_IP)/" ${BITOPS_ENVROOT}/terraform/inventory.yaml
