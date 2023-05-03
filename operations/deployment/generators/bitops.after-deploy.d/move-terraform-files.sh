80800c917312df33ac3cff552cfe90843ace07ff
LeoDiazL <leo@bitovi.com>
2023-05-03 10:09:16 -0300

    EFS AZ move

--- operations/deployment/generators/bitops.after-deploy.d/move-terraform-files.sh
+++ operations/deployment/generators/bitops.after-deploy.d/move-terraform-files.sh
@@ -8,7 +8,7 @@ rm "${BITOPS_ENVROOT}/generators/generator.tf"
 rm "${BITOPS_ENVROOT}/generators/variables.tf"
 rm "${BITOPS_ENVROOT}/generators/terraform.tfvars"
 mv "${BITOPS_ENVROOT}"/generators/bitovi_*postgres.tf "${BITOPS_ENVROOT}"/terraform/rds/.
-cp "${BITOPS_ENVROOT}"/generators/bitovi_aws_default_tags* "${BITOPS_ENVROOT}"/terraform/rds/.
+cp "${BITOPS_ENVROOT}"/generators/bitovi_aws_default* "${BITOPS_ENVROOT}"/terraform/rds/.
 mv "${BITOPS_ENVROOT}"/generators/bitovi_*.tf "${BITOPS_ENVROOT}"/terraform/ec2/.
 rm -rf "${BITOPS_ENVROOT}/generators"
 cp -r "${BITOPS_ENVROOT}" /opt/bitops_deployment/generated_code
--- operations/deployment/generators/modules/aws/ec2/ec2.tf
+++ operations/deployment/generators/modules/aws/ec2/ec2.tf
@@ -1,3 +1,7 @@
+resource "local_file" "aws_ec2_azs_efs" {
+    filename = format("%s/%s", abspath(path.root), "bitovi_aws_ec2_azs_efs.tf")
+    content = file(format("%s/%s", abspath(path.module), "aws_ec2_azs_efs.tmpl"))
+}
 resource "local_file" "aws_ec2_iam_profile" {
     filename = format("%s/%s", abspath(path.root), "bitovi_aws_ec2_iam_profile.tf")
     content = file(format("%s/%s", abspath(path.module), "aws_ec2_iam_profile.tmpl"))
