252ef74d086fe0827a3c0a83a6654256c65f130f
LeoDiazL <leo@bitovi.com>
2023-05-04 12:45:11 -0300

    Missing file in commit

--- operations/deployment/generators/modules/aws/efs/efs.tf
+++ operations/deployment/generators/modules/aws/efs/efs.tf
@@ -2,8 +2,3 @@ resource "local_file" "aws_efs" {
     filename = format("%s/%s", abspath(path.root), "bitovi_aws_efs.tf")
     content = file(format("%s/%s", abspath(path.module), "aws_efs.tmpl"))
 }
-
-resource "local_file" "aws_dotenv_efs" {
-    filename = format("%s/%s", abspath(path.root), "bitovi_aws_dotenv_efs.tf")
-    content  = file(format("%s/%s", abspath(path.module), "aws_dotenv_efs.tmpl"))
-}
