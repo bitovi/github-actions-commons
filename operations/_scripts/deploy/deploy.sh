e81eefd9e300f3118410eeb68d43322155d7f404
LeoDiazL <leo@bitovi.com>
2023-05-05 13:31:23 -0300

    Debug

--- operations/_scripts/deploy/deploy.sh
+++ operations/_scripts/deploy/deploy.sh
@@ -86,14 +86,27 @@ fi
 echo "Final BitOps config file"
 cat $GITHUB_ACTION_PATH/operations/deployment/bitops.config.yaml
 
+echo "Pre"
+echo "$TF_STATE_BUCKET_DESTROY"
+echo "$TF_STACK_DESTROY"
+echo "$AWS_POSTGRES_ENABLE"
+echo "$AWS_EFS_ENABLE"
+echo "$AWS_EC2_INSTANCE_CREATE"
+
 ## Ensuring bucket get's destroyed only if everything is set to be destroyed
-if [[ $(alpha_only "$TF_STATE_BUCKET_DESTROY") == true ]] && ! [[ $(alpha_only "$TF_STACK_DESTROY") == "true" ]] ; then
+if [[ $(alpha_only "$TF_STATE_BUCKET_DESTROY") == true ]] && ! [[ $(alpha_only "$TF_STACK_DESTROY") == true ]] ; then
   if [[ $(alpha_only "$AWS_POSTGRES_ENABLE") == true ]] || 
      [[ $(alpha_only "$AWS_EFS_ENABLE") == true ]] || 
      [[ $(alpha_only "$AWS_EC2_INSTANCE_CREATE") == true ]]; then 
     export TF_STATE_BUCKET_DESTROY="false"
   fi
 fi
+echo "Post"
+echo "$TF_STATE_BUCKET_DESTROY"
+echo "$TF_STACK_DESTROY"
+echo "$AWS_POSTGRES_ENABLE"
+echo "$AWS_EFS_ENABLE"
+echo "$AWS_EC2_INSTANCE_CREATE"
 
 # Generating GitHub Variables and Secrets files
 mkdir -p "${GITHUB_ACTION_PATH}/operations/deployment/env-files"
@@ -107,6 +120,7 @@ if [[ $SKIP_BITOPS_RUN == "true" ]]; then
   exit 1
 fi
 
+exit 0
 echo "::group::BitOps Excecution"  
 echo "Running BitOps for env: $BITOPS_ENVIRONMENT"
 docker run --rm --name bitops \
