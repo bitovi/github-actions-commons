777615ed176ac09d67493697c0089f654ef6788d
LeoDiazL <leo@bitovi.com>
2023-05-05 12:01:54 -0300

    Fix path

--- operations/_scripts/generate/generate_app_repo.sh
+++ operations/_scripts/generate/generate_app_repo.sh
@@ -9,11 +9,10 @@ if [ -n "$DOCKER_REPO_APP_DIRECTORY" ]; then
     TARGET_PATH="${TARGET_PATH}/${DOCKER_REPO_APP_DIRECTORY}"
 fi
 
-mkdir -p "${GITHUB_ACTION_PATH}/operations/deployment/$1/app"
+mkdir -p "${GITHUB_ACTION_PATH}/operations/deployment/$1/app/${GITHUB_REPO_NAME}"
 
 if [ $(find "$TARGET_PATH/." -iname "*"  -not -name "."| wc -l) -gt 0 ]; then 
   echo "Copying files from GITHUB_WORKSPACE ($GITHUB_WORKSPACE) to ops repo's Ansible $1 deployment (${GITHUB_ACTION_PATH}/operations/deployment/$1/app/${GITHUB_REPO_NAME})"
-  mkdir -p "${GITHUB_ACTION_PATH}/operations/deployment/$1/app/${GITHUB_REPO_NAME}"
   cp -rf "$TARGET_PATH"/* "${GITHUB_ACTION_PATH}/operations/deployment/$1/app/${GITHUB_REPO_NAME}/"
 else 
   echo "Nothing to copy from repo"
