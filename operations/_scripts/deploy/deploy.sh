22eda11583c4b8f986d764f80a3adf5da810b76f
LeoDiazL <leo@bitovi.com>
2023-05-05 10:55:06 -0300

    Debuggg

--- operations/_scripts/deploy/deploy.sh
+++ operations/_scripts/deploy/deploy.sh
@@ -40,12 +40,12 @@ if [ "$(alpha_only $ANSIBLE_SKIP)" == "true" ]; then
   ANSIBLE_SKIP="true"
 fi
 
-# Check EFS 
-#if [[ $(alpha_only "$AWS_EFS_CREATE") == true ]] || [[ $(alpha_only "$AWS_EFS_CREATE_HA") == true ]] || [ -n "$AWS_EFS_MOUNT_ID" ]; then 
-#  export AWS_EFS_ENABLE="true"
-#else
-#  export AWS_EFS_ENABLE="false"
-#fi
+# Adding global EFS flag
+if [[ $(alpha_only "$AWS_EFS_CREATE") == true ]] || [[ $(alpha_only "$AWS_EFS_CREATE_HA") == true ]] || [ -n "$AWS_EFS_MOUNT_ID" ]; then 
+  export AWS_EFS_ENABLE="true"
+else
+  export AWS_EFS_ENABLE="false"
+fi
 
 # Generate buckets identifiers and check them agains AWS Rules 
 export TF_STATE_BUCKET="$(/bin/bash $GITHUB_ACTION_PATH/operations/_scripts/generate/generate_buckets_identifiers.sh tf | xargs)"
@@ -86,24 +86,16 @@ fi
 echo "Final BitOps config file"
 cat $GITHUB_ACTION_PATH/operations/deployment/bitops.config.yaml
 
-# Debugging states
-# Ensuring bucket get's destroyed only if everything is set to be destroyed
-#echo "PRE CHECK"
-#echo "$TF_STATE_BUCKET_DESTROY"
-#echo "$AWS_POSTGRES_ENABLE"
-#echo "$AWS_EFS_ENABLE"
-#echo "$AWS_EC2_INSTANCE_CREATE"
-
 ## Ensuring bucket get's destroyed only if everything is set to be destroyed
-#if [[ $(alpha_only "$TF_STATE_BUCKET_DESTROY") == true ]]; then
-#  echo "Got in the first if"
-#  if [[ $(alpha_only "$AWS_POSTGRES_ENABLE") == true ]] || 
-#     [[ $(alpha_only "$AWS_EFS_ENABLE") == true ]] || 
-#     [[ $(alpha_only "$AWS_EC2_INSTANCE_CREATE") == true ]]; then 
-#    export TF_STATE_BUCKET_DESTROY="false"
-#    echo "Bucket should be set to FALSE!"
-#  fi
-#fi
+if [[ $(alpha_only "$TF_STATE_BUCKET_DESTROY") == true ]]; then
+  echo "Got in the first if"
+  if [[ $(alpha_only "$AWS_POSTGRES_ENABLE") == true ]] || 
+     [[ $(alpha_only "$AWS_EFS_ENABLE") == true ]] || 
+     [[ $(alpha_only "$AWS_EC2_INSTANCE_CREATE") == true ]]; then 
+    export TF_STATE_BUCKET_DESTROY="false"
+    echo "Bucket should be set to FALSE!"
+  fi
+fi
 
 # Debugging states
 # Ensuring bucket get's destroyed only if everything is set to be destroyed
