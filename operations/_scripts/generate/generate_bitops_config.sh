2007c06f96d2ba2e1a218276ce1430d6439ceb6a
LeoDiazL <leo@bitovi.com>
2023-05-08 17:19:55 -0300

    Debug 9

--- operations/_scripts/generate/generate_bitops_config.sh
+++ operations/_scripts/generate/generate_bitops_config.sh
@@ -68,19 +68,9 @@ if [[ $(alpha_only "$AWS_EC2_INSTANCE_CREATE") == true ]]; then
       pwd
       find "/home/runner/work/." -iname "echo.sh"
       ls -lah $GITHUB_WORKSPACE
-      echo "example"
-      ls -lah /home/runner/work/devops-training-ec2-gha-example/
-      echo "work"
-      ls -lah /home/runner/work/
-      echo "env"
-      env
       echo "something"
-      cd /home/runner/work 
-      find . -iname echo.sh
-      ls -lah /home/runner/work/_actions
-      ls -lah /home/runner/work/_actions/bitovi/github-actions-commons/terraform_split
       ls -lah "$GH_ACTION_REPO"
-      
+
    #   mv "$GITHUB_WORKSPACE/$AWS_EC2_USER_DATA_FILE" "$GITHUB_ACTION_PATH/operations/deployment/terraform/ec2/aws_ec2_incoming_user_data_script.sh"
   #fi
 fi
