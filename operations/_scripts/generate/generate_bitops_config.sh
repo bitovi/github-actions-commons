45e2c3cfebafd43d9316afc5461f7a1d522b61e7
LeoDiazL <leo@bitovi.com>
2023-05-08 16:40:47 -0300

    Debug 4

--- operations/_scripts/generate/generate_bitops_config.sh
+++ operations/_scripts/generate/generate_bitops_config.sh
@@ -65,6 +65,8 @@ if [[ $(alpha_only "$AWS_EC2_INSTANCE_CREATE") == true ]]; then
       echo "Cating user data file"
       #cat "$GITHUB_WORKSPACE/$AWS_EC2_USER_DATA_FILE"
       echo "LS user data file"
+      pwd
+      find "/home/runner/work/." -iname "echo.sh"
       ls -lah $GITHUB_WORKSPACE
    #   mv "$GITHUB_WORKSPACE/$AWS_EC2_USER_DATA_FILE" "$GITHUB_ACTION_PATH/operations/deployment/terraform/ec2/aws_ec2_incoming_user_data_script.sh"
   #fi
