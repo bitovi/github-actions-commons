13bb45d080ae7c20d65ed8a62b9732f9f1f3d24a
LeoDiazL <leo@bitovi.com>
2023-05-12 09:54:37 -0300

    appending tmpl to template variables - fix missing $

--- operations/deployment/generators/modules/aws/elb/elb.tf
+++ operations/deployment/generators/modules/aws/elb/elb.tf
@@ -1,8 +1,8 @@
 resource "local_file" "aws_elb" {
     filename = format("%s/%s", abspath(path.root), "bitovi_aws_elb.tf")
     content  = templatefile(format("%s/%s", abspath(path.module), "aws_elb.tmpl"), {
-      elb_resource_string = var.aws_r53_enable_cert ? "vm_lb_ssl" : "vm_lb"
-      cert_string = var.aws_r53_enable_cert ? "$${local.selected_arn}" : ""
+      tmpl_elb_resource_string = var.aws_r53_enable_cert ? "vm_lb_ssl" : "vm_lb"
+      tmpl_cert_string = var.aws_r53_enable_cert ? "$${local.selected_arn}" : ""
     })
 }
 
