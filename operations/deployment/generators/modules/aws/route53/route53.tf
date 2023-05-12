76b0283f315be02a9df2417c09debaaa346174b7
LeoDiazL <leo@bitovi.com>
2023-05-12 09:54:47 -0300

    Adding missing files

--- operations/deployment/generators/modules/aws/route53/route53.tf
+++ operations/deployment/generators/modules/aws/route53/route53.tf
@@ -1,8 +1,8 @@
 resource "local_file" "aws_route53" {
     filename = format("%s/%s", abspath(path.root), "bitovi_aws_route53.tf")
     content  = templatefile(format("%s/%s", abspath(path.module), "aws_route53.tmpl"), {
-      elb_resource_string = var.aws_r53_enable_cert ? "vm_lb_ssl" : "vm_lb"
-      r53_protocol_string = var.aws_r53_enable_cert ? "local.selected_arn != \"\" ? \"https://\" : \"http://\"" : "\"http://\""
+      tmpl_elb_resource_string = var.aws_r53_enable_cert ? "vm_lb_ssl" : "vm_lb"
+      tmpl_r53_protocol_string = var.aws_r53_enable_cert ? "local.selected_arn != \"\" ? \"https://\" : \"http://\"" : "\"http://\""
     })
 }
 
