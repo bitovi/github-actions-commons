125e26a132766f521b13939bffead0520bc7874a
LeoDiazL <leo@bitovi.com>
2023-05-18 10:10:11 -0300

    Debuggings

--- operations/deployment/terraform/ec2/bitops.after-deploy.d/generate-outputs.sh
+++ operations/deployment/terraform/ec2/bitops.after-deploy.d/generate-outputs.sh
@@ -11,10 +11,7 @@
 #   The bo-out.env file is used by Ansible to populate variables passed on by Terraform
 # """
 
-set -x
-
 echo "In afterhook - generate-outputs.sh"
-echo "ENV After Terra"
 TARGET_FILE="/opt/bitops_deployment/bo-out.env"
 
 if [ "$BITOPS_TERRAFORM_COMMAND" != "destroy" ]; then
@@ -34,8 +31,5 @@ AWS_INSTANCE_URL="$BITOPS_EC2_PUBLIC_URL"
       sed -i "s/BITOPS_EC2_PUBLIC_IP/$(echo $BITOPS_EC2_PUBLIC_IP)/" ${BITOPS_ENVROOT}/terraform/ec2/inventory.yaml
   fi
 fi
-
-ls -lah /opt/bitops_deployment
-mount
 
 echo "end terraform output for bo-out"
