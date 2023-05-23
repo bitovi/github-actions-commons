f406b7d05d7b4f0db6a4df09189c68d62f2ec9cb
LeoDiazL <116020815+LeoDiazL@users.noreply.github.com>
2023-05-23 17:43:57 -0300

    Include dotted files (#23)
    * Initial commit
    * Typo

--- operations/_scripts/generate/generate_app_repo.sh
+++ operations/_scripts/generate/generate_app_repo.sh
@@ -14,8 +14,8 @@ mkdir -p "${GITHUB_ACTION_PATH}/operations/deployment/$1/app/${GITHUB_REPO_NAME}
 
 if [ $(find "$TARGET_PATH/." -iname "*"  -not -name "."| wc -l) -gt 0 ]; then 
   echo "Copying files from $TARGET_PATH to ops repo's Ansible $1 deployment (${GITHUB_ACTION_PATH}/operations/deployment/$1/app/${GITHUB_REPO_NAME})"
-  if [ -f "$TARGET_PATH/.ignore" ]; then
-    rsync -a --exclude-from="$TARGET_PATH/.ignore" "$TARGET_PATH"/ "${GITHUB_ACTION_PATH}/operations/deployment/$1/app/${GITHUB_REPO_NAME}/"
+  if [ -f "$TARGET_PATH/.gha-ignore" ]; then
+    rsync -a --exclude-from="$TARGET_PATH/.gha-ignore" "$TARGET_PATH"/ "${GITHUB_ACTION_PATH}/operations/deployment/$1/app/${GITHUB_REPO_NAME}/"
   else
     rsync -a "$TARGET_PATH"/ "${GITHUB_ACTION_PATH}/operations/deployment/$1/app/${GITHUB_REPO_NAME}/"
   fi
