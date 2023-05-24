6c8ddf268350a10e24a63d68609266075829b8e4
LeoDiazL <leo@bitovi.com>
2023-05-24 18:19:08 -0300

    Fixing move after generation

--- operations/deployment/generators/bitops.after-deploy.d/move-terraform-files.sh
+++ operations/deployment/generators/bitops.after-deploy.d/move-terraform-files.sh
@@ -21,8 +21,8 @@ if [ $(find "$BITOPS_ENVROOT/generators/." -iname "bitovi_aws_efs.tf" | wc -l) -
   mv "${BITOPS_ENVROOT}"/generators/bitovi_aws_efs.tf "${BITOPS_ENVROOT}"/terraform/efs/.
   cp "${BITOPS_ENVROOT}"/generators/bitovi_aws_default* "${BITOPS_ENVROOT}"/terraform/efs/.
 fi
-if [ $(find "$BITOPS_ENVROOT/generators/." -iname "bitovi_aws_eks.tf" | wc -l) -gt 0 ]; then 
-  mv "${BITOPS_ENVROOT}"/generators/bitovi_aws_eks.tf "${BITOPS_ENVROOT}"/terraform/eks/.
+if [ $(find "$BITOPS_ENVROOT/generators/." -iname "bitovi_aws_eks*.tf" | wc -l) -gt 0 ]; then 
+  mv "${BITOPS_ENVROOT}"/generators/bitovi_aws_eks*.tf "${BITOPS_ENVROOT}"/terraform/eks/.
   cp "${BITOPS_ENVROOT}"/generators/bitovi_aws_default* "${BITOPS_ENVROOT}"/terraform/eks/.
 fi
 if [ $(find "$BITOPS_ENVROOT/generators/." -iname "bitovi_*.tf" | wc -l) -gt 0 ]; then 
