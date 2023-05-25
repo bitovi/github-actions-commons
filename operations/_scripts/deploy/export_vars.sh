65a4888acb5ae1c82224906d05aab04ac6f09e01
LeoDiazL <leo@bitovi.com>
2023-05-25 12:56:29 -0300

    DEVOPS-787 - in testing

--- operations/_scripts/deploy/export_vars.sh
+++ operations/_scripts/deploy/export_vars.sh
@@ -2,9 +2,8 @@
 # Export variables to GHA
 
 echo "::group::Passing vars to GH"
-if [ "$TF_STACK_DESTROY" != "true" ]; then
+if [[ "$TF_STACK_DESTROY" != "true" ]]; then
   BO_OUT="$GITHUB_ACTION_PATH/operations/bo-out.env"
-  cat $BO_OUT
   echo "Check for $BO_OUT"
   if [ -s $BO_OUT ]; then
     echo "Outputting bo-out.env to GITHUB_OUTPUT"
