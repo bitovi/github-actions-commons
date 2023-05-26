46c70db93864707440130083d1cbded640d6e767
LeoDiazL <leo@bitovi.com>
2023-05-26 10:53:02 -0300

    Changing the conditionals

--- operations/deployment/terraform/eks/bitops.after-deploy.d/helm-install.sh
+++ operations/deployment/terraform/eks/bitops.after-deploy.d/helm-install.sh
@@ -16,15 +16,23 @@ function install_charts (){
   source_folder="$1"
   if [ -d $source_folder ]; then
     # Move files from source folder to destination folder
-    find "$source_folder" -maxdepth 1 -type f -path "$source_folder/*.tgz" | while read file; do
-      echo "Installing $file"
-      helm install $(remove_extension "$file") $source_folder/$file
+    for chart in $source_folder/*.tgz; do
+      echo "Installing chart: $chart"
+      echo "helm install $(remove_extension $chart) $source_folder/$chart"
     done
-    # Move remaining folders (if they exist) and exclude the . folder
-    find "$source_folder" -maxdepth 1 -type d -not -name "." -path "$source_folder/*" | while read folder; do
-      echo "Installing $folder"
-      helm install $folder $source_folder/$folder
+    for chart in $(find "$source_folder" -type d -maxdepth 1); do
+      echo "Installing chart: $chart"
+      echo "helm install $chart $source_folder/$chart"
     done
+    #find "$source_folder" -maxdepth 1 -type f -path "$source_folder/*.tgz" | while read file; do
+    #  echo "Installing $file"
+    #  helm install $(remove_extension "$file") $source_folder/$file
+    #done
+    ## Move remaining folders (if they exist) and exclude the . folder
+    #find "$source_folder" -maxdepth 1 -type d -not -name "." -path "$source_folder/*" | while read folder; do
+    #  echo "Installing $folder"
+    #  helm install $folder $source_folder/$folder
+    #done
   fi
 }
 
