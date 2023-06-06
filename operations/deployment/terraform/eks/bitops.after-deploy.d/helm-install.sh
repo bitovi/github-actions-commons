edaec6debb6a22b3222bb89186d7d63732961ca6
LeoDiazL <leo@bitovi.com>
2023-06-06 15:47:47 -0300

    Debug helm-install

--- operations/deployment/terraform/eks/bitops.after-deploy.d/helm-install.sh
+++ operations/deployment/terraform/eks/bitops.after-deploy.d/helm-install.sh
@@ -16,6 +16,7 @@ function remove_extension() {
 
 function install_charts (){
   source_folder="$1"
+  echo "Running through $source_folder"
   if [ -d $source_folder ]; then
     # Move files from source folder to destination folder
     if [ $(find ${source_folder} -maxdepth 1 -type f -iname "*.tgz" | wc -l) -gt 0 ]; then
@@ -35,29 +36,30 @@ function install_charts (){
 
 if [ "$BITOPS_TERRAFORM_COMMAND" != "destroy" ]; then
   aws eks update-kubeconfig --name eks-cluster
+  tree "${BITOPS_ENVROOT}/terraform/eks/helm-charts"
   install_charts "${BITOPS_ENVROOT}/terraform/eks/helm-charts/deployment-charts"
   install_charts "${BITOPS_ENVROOT}/terraform/eks/helm-charts/action-charts"
-  
+
   ## DEBUG
   echo "kubectl describe after charts installation"
-  echo "kubectl get configmaps --all-namespaces"
-  kubectl get configmaps --all-namespaces
-  echo "kubectl get namespaces"
-  kubectl get namespaces
-  echo "kubectl get deployments --all-namespaces"
-  kubectl get deployments --all-namespaces
-  echo "kubectl get configmaps --all-namespaces"
-  kubectl get configmap aws-auth -n default -o yaml
-  echo "AWS Suggestions"
-  echo "###############"
-  echo "kubectl describe -n default configmap/aws-auth"
-  kubectl describe -n default configmap/aws-auth
-  echo "kubectl get roles -A"
-  kubectl get roles -A
-  echo "kubectl get clusterroles"
-  kubectl get clusterroles
-  echo "kubectl get rolebindings -A"
-  kubectl get rolebindings -A
-  echo "kubectl get clusterrolebindings"
-  kubectl get clusterrolebindings
+  # echo "kubectl get configmaps --all-namespaces"
+  # kubectl get configmaps --all-namespaces
+  # echo "kubectl get namespaces"
+  # kubectl get namespaces
+  # echo "kubectl get deployments --all-namespaces"
+  # kubectl get deployments --all-namespaces
+  # echo "kubectl get configmaps --all-namespaces"
+  # kubectl get configmap aws-auth -n default -o yaml
+  # echo "AWS Suggestions"
+  # echo "###############"
+  # echo "kubectl describe -n default configmap/aws-auth"
+  # kubectl describe -n default configmap/aws-auth
+  # echo "kubectl get roles -A"
+  # kubectl get roles -A
+  # echo "kubectl get clusterroles"
+  # kubectl get clusterroles
+  # echo "kubectl get rolebindings -A"
+  # kubectl get rolebindings -A
+  # echo "kubectl get clusterrolebindings"
+  # kubectl get clusterrolebindings
 fi
