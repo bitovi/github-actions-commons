31df585a14a9c1485b4a2e3df0de28213fb169e9
LeoDiazL <leo@bitovi.com>
2023-06-23 10:05:22 -0300

    Initial moving commit

--- operations/deployment/generators/modules/aws/efs/efs.tf
+++
@@ -1,4 +0,0 @@
-resource "local_file" "aws_efs" {
-    filename = format("%s/%s", abspath(path.root), "bitovi_aws_efs.tf")
-    content = file(format("%s/%s", abspath(path.module), "aws_efs.tmpl"))
-}
--- operations/deployment/generators/modules/aws/rds/rds.tf
+++
@@ -1,9 +0,0 @@
-resource "local_file" "aws_dotenv_postgres" {
-    filename = format("%s/%s", abspath(path.root), "bitovi_aws_dotenv_postgres.tf")
-    content = file(format("%s/%s", abspath(path.module), "aws_dotenv_postgres.tmpl"))
-}
-
-resource "local_file" "aws_postgres" {
-    filename = format("%s/%s", abspath(path.root), "bitovi_aws_postgres.tf")
-    content = file(format("%s/%s", abspath(path.module), "aws_postgres.tmpl"))
-}
