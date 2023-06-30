7fbd7c4f0b1723289bb84c02b78ac963aa3bcaa9
mcascone <max@bitovi.com>
2023-06-30 13:19:08 -0500

    simplify logic

--- operations/_scripts/deploy/summary.sh
+++ operations/_scripts/deploy/summary.sh
@@ -15,25 +15,25 @@ if [[ $SUCCESS == 'true' ]]; then
     result_string="
     ## VM Created! :rocket:
     $URL_OUTPUT"
-  elif [[ $URL_OUTPUT == '' && $BITOPS_CODE_ONLY == 'true' && $BITOPS_CODE_STORE == 'true' ]]; then
+  elif [[ $BITOPS_CODE_ONLY == 'true' && $BITOPS_CODE_STORE == 'true' ]]; then
     #Print code generated and archived
     result_string="## BitOps Code generated. :tada: 
     Download the code artifact. Will be there for 5 days.
     Keep in mind that for creation, EFS should be created before EC2.
     While destroying, EC2 should be destroyed before EFS. (Due to resources being in use).
     You can change that in the bitops.config.yaml file, or regenerate the code with destroy set."
-  elif [[ $URL_OUTPUT == '' && $BITOPS_CODE_ONLY == 'true' && $BITOPS_CODE_STORE != 'true' ]]; then
+  elif [[ $BITOPS_CODE_ONLY == 'true' && $BITOPS_CODE_STORE != 'true' ]]; then
     #Print code generated not archived
     result_string="## BitOps Code generated. :tada:"
-  elif [[ $URL_OUTPUT == '' && $TF_STACK_DESTROY != 'true' && $BITOPS_CODE_ONLY != 'true' ]]; then
+  elif [[ $TF_STACK_DESTROY != 'true' && $BITOPS_CODE_ONLY != 'true' ]]; then
     #Print result deploy finished but no URL
     result_string="## Deploy finished! But no URL found. :thinking:
     If expecting a URL, please check the logs for possible  errors.
     If you consider this is a bug in the Github Action, please submit an issue to our repo."
-  elif [[ $URL_OUTPUT == '' && $TF_STACK_DESTROY == 'true' && $TF_STATE_BUCKET_DESTROY != 'true' ]]; then
+  elif [[ $TF_STACK_DESTROY == 'true' && $TF_STATE_BUCKET_DESTROY != 'true' ]]; then
     result_string="## VM Destroyed! :boom:
     Infrastructure should be gone now!"
-  elif [[ $URL_OUTPUT == '' && $TF_STACK_DESTROY == 'true' && $TF_STATE_BUCKET_DESTROY == 'true' ]]; then
+  elif [[ $TF_STACK_DESTROY == 'true' && $TF_STATE_BUCKET_DESTROY == 'true' ]]; then
     result_string="## VM Destroyed! :boom:
     Buckets and infrastructure should be gone now!"
   fi
