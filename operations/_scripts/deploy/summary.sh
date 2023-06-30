acbe6ae319841b3549b47b1a2e22ebbf63e6321f
mcascone <max@bitovi.com>
2023-06-30 13:19:08 -0500

    simple multilines better than heredocs

--- operations/_scripts/deploy/summary.sh
+++ operations/_scripts/deploy/summary.sh
@@ -9,50 +9,39 @@
 # TF_STACK_DESTROY
 # TF_STATE_BUCKET_DESTROY
 
-if [[ $SUCCESS == 'true' && $URL_OUTPUT != '' ]]; then
-  #Print result created
-  read -r -d '' result_string << EOF
-  ## VM Created! :rocket:
-  $URL_OUTPUT
-EOF
-elif [[ $SUCCESS == 'true' && $URL_OUTPUT == '' && $BITOPS_CODE_ONLY == 'true' && $BITOPS_CODE_STORE == 'true' ]]; then
-  #Print code generated and archived
-  read -r -d '' result_string << EOF
-  ## BitOps Code generated. :tada: 
-  Download the code artifact. Will be there for 5 days.
-  Keep in mind that for creation, EFS should be created before EC2.
-  While destroying, EC2 should be destroyed before EFS. (Due to resources being in use).
-  You can change that in the bitops.config.yaml file, or regenerate the code with destroy set.
-EOF
-elif [[ $SUCCESS == 'true' && $URL_OUTPUT == '' && $BITOPS_CODE_ONLY == 'true' && $BITOPS_CODE_STORE != 'true' ]]; then
-  #Print code generated not archived
-  read -r -d '' result_string << EOF
-  ## BitOps Code generated. :tada:
-EOF
-elif [[ $SUCCESS == 'true' && $URL_OUTPUT == '' && $TF_STACK_DESTROY != 'true' && $BITOPS_CODE_ONLY != 'true' ]]; then
-  #Print result deploy finished but no URL
-  read -r -d '' result_string << EOF
-  ## Deploy finished! But no URL found. :thinking:
-  If expecting an URL, please check the logs for possible  errors.
-  If you consider this is a bug in the Github Action, please submit an issue to our repo.
-EOF
-elif [[ $SUCCESS == 'true' && $URL_OUTPUT == '' && $TF_STACK_DESTROY == 'true' && $TF_STATE_BUCKET_DESTROY != 'true' ]]; then
-  read -r -d '' result_string << EOF
-  ## VM Destroyed! :boom:
-  Infrastructure should be gone now!
-EOF
-elif [[ $SUCCESS == 'true' && $URL_OUTPUT == '' && $TF_STACK_DESTROY == 'true' && $TF_STATE_BUCKET_DESTROY == 'true' ]]; then
-  read -r -d '' result_string << EOF
-  ## VM Destroyed! :boom:
-  Buckets and infrastructure should be gone now!
-EOF
-elif [[ $SUCCESS != 'true' ]]; then
+if [[ $SUCCESS == 'true' ]]; then 
+  if [[ $URL_OUTPUT != '' ]]; then
+    #Print result created
+    result_string="
+    ## VM Created! :rocket:
+    $URL_OUTPUT"
+  elif [[ $URL_OUTPUT == '' && $BITOPS_CODE_ONLY == 'true' && $BITOPS_CODE_STORE == 'true' ]]; then
+    #Print code generated and archived
+    result_string="## BitOps Code generated. :tada: 
+    Download the code artifact. Will be there for 5 days.
+    Keep in mind that for creation, EFS should be created before EC2.
+    While destroying, EC2 should be destroyed before EFS. (Due to resources being in use).
+    You can change that in the bitops.config.yaml file, or regenerate the code with destroy set."
+  elif [[ $URL_OUTPUT == '' && $BITOPS_CODE_ONLY == 'true' && $BITOPS_CODE_STORE != 'true' ]]; then
+    #Print code generated not archived
+    result_string="## BitOps Code generated. :tada:"
+  elif [[ $URL_OUTPUT == '' && $TF_STACK_DESTROY != 'true' && $BITOPS_CODE_ONLY != 'true' ]]; then
+    #Print result deploy finished but no URL
+    result_string="## Deploy finished! But no URL found. :thinking:
+    If expecting a URL, please check the logs for possible  errors.
+    If you consider this is a bug in the Github Action, please submit an issue to our repo."
+  elif [[ $URL_OUTPUT == '' && $TF_STACK_DESTROY == 'true' && $TF_STATE_BUCKET_DESTROY != 'true' ]]; then
+    result_string="## VM Destroyed! :boom:
+    Infrastructure should be gone now!"
+  elif [[ $URL_OUTPUT == '' && $TF_STACK_DESTROY == 'true' && $TF_STATE_BUCKET_DESTROY == 'true' ]]; then
+    result_string="## VM Destroyed! :boom:
+    Buckets and infrastructure should be gone now!"
+  fi
+else
   # Print error result
-  read -r -d '' result_string << EOF
-  ## Workflow failed to run :fire:
+  result_string="## Workflow failed to run :fire:
   Please check the logs for possible errors.
-  If you consider this is a bug in the Github Action, please submit an issue to our repo.
-EOF
+  If you consider this is a bug in the Github Action, please submit an issue to our repo."
 fi
 
 echo "$result_string" >> $GITHUB_OUTPUT
