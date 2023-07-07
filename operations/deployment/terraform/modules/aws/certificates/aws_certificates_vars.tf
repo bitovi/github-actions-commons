9c279ae88f6bf8ce7667054c39cd1b2d89438025
LeoDiazL <leo@bitovi.com>
2023-07-07 11:00:44 -0300

    Fixing var type

--- operations/deployment/terraform/modules/aws/certificates/aws_certificates_vars.tf
+++ operations/deployment/terraform/modules/aws/certificates/aws_certificates_vars.tf
@@ -5,7 +5,9 @@ variable "aws_r53_cert_arn" {}
 variable "aws_r53_domain_name" {}
 variable "aws_r53_sub_domain_name" {}
 # Others
-variable "aws_route53_zone_id" {}
+variable "aws_route53_zone_id" {
+    type = list
+}
 variable "fqdn_provided" {}
 variable "common_tags" {
     type = map
