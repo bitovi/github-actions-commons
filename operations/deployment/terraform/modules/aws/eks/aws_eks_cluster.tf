583b0a103c7b56ba0a2ff8f5df5c48bb490ab711
LeoDiazL <leo@bitovi.com>
2023-07-10 15:53:05 -0300

    Commenting out kubernetes provider from eks module

--- operations/deployment/terraform/modules/aws/eks/aws_eks_cluster.tf
+++ operations/deployment/terraform/modules/aws/eks/aws_eks_cluster.tf
@@ -36,11 +36,11 @@ data "aws_eks_cluster_auth" "cluster_auth" {
   name = aws_eks_cluster.main.id
 }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks_cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster_auth.token
-}
+#provider "kubernetes" {
+#  host                   = data.aws_eks_cluster.eks_cluster.endpoint
+#  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority.0.data)
+#  token                  = data.aws_eks_cluster_auth.cluster_auth.token
+#}
 
 resource "aws_launch_template" "main" {
   network_interfaces {
