8ee6dc7fd99246979f52d4b240f5f6af69380c92
LeoDiazL <leo@bitovi.com>
2023-07-14 12:10:12 -0300

    Code cleanup

--- operations/deployment/generators-tbd/generator.tf
+++
@@ -1,4 +0,0 @@
-module "ec2" {
-  source = "./modules/aws/ec2"
-  aws_ec2_ami_update = var.aws_ec2_ami_update
-}
--- operations/deployment/generators-tbd/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "aws_ec2_ami_update" {
-  type        = bool
-  description = "Recreates the EC2 instance if there is a newer version of the AMI"
-  default     = false
-}
