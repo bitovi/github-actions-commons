4b25059be6ce43bb59e06686ef7b40a02ad88a82
LeoDiazL <leo@bitovi.com>
2023-07-24 14:09:46 -0300

    Adding try

--- operations/deployment/terraform/aws/aws_eks_defaults.tf
+++ operations/deployment/terraform/aws/aws_eks_defaults.tf
@@ -1,13 +1,13 @@
 data "aws_eks_cluster" "eks_cluster" {
-  name = module.eks[0].aws_eks_cluster_main_id
+  name = try(module.eks[0].aws_eks_cluster_main_id,"")
 }
 
 data "aws_eks_cluster_auth" "cluster_auth" {
-  name = module.eks[0].aws_eks_cluster_main_id
+  name = try(module.eks[0].aws_eks_cluster_main_id,"")
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks_cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster_auth.token
+  host                   = try(data.aws_eks_cluster.eks_cluster.endpoint,"")
+  cluster_ca_certificate = try(base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority.0.data),"")
+  token                  = try(data.aws_eks_cluster_auth.cluster_auth.token,"")
 }
