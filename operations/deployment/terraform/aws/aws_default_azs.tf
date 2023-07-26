387e2e9e76210c5e50e8463576755a61d36db0b5
LeoDiazL <leo@bitovi.com>
2023-07-26 17:26:56 -0300

    typo 2

--- operations/deployment/terraform/aws/aws_default_azs.tf
+++ operations/deployment/terraform/aws/aws_default_azs.tf
@@ -71,7 +71,7 @@ locals {
   # no_zone_mapping: Creates a empty zone mapping object list
   no_zone_mapping  = { "" : { "subnet_id" : "", "security_groups" : [""] } }
   # ec2_zone_mapping: Creates a zone mapping object list based on default values (default sg, default subnet, etc)
-  ec2_zone_mapping =  { "${local.preferred_az}" : { "subnet_id" : "${data.aws_subnet.selected[0].id}", "security_groups" : [module.ec2[0].aws_security_group_ec2_sg_name] }
+  ec2_zone_mapping =  { "${local.preferred_az}" : { "subnet_id" : "${data.aws_subnet.selected[0].id}", "security_groups" : [module.ec2[0].aws_security_group_ec2_sg_name] } }
 
   # auto_ha_availability_zone*: Creates zone map objects for each available AZ in a region
   auto_ha_availability_zonea = {
@@ -110,7 +110,7 @@ locals {
     }
   }) : null
   # ha_zone_mapping: Creates a zone mapping object list for all available AZs in a region
-  ha_zone_mapping = merge(local.auto_ha_availability_zonea, local.auto_ha_availability_zoneb, local.auto_ha_availability_zonec, local.auto_ha_availability_zoned, local.auto_ha_availability_zonee, local.auto_ha_availability_zonef) }
+  ha_zone_mapping = merge(local.auto_ha_availability_zonea, local.auto_ha_availability_zoneb, local.auto_ha_availability_zonec, local.auto_ha_availability_zoned, local.auto_ha_availability_zonee, local.auto_ha_availability_zonef)
 }
 
 #output "ec2_zone_mapping" {
