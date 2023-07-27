b284191a70eab469651529ea37e69cb8a5395b68
LeoDiazL <leo@bitovi.com>
2023-07-27 16:22:24 -0300

    Fixing out_dotenv

--- operations/deployment/terraform/modules/aws/aurora/aws_dotenv_aurora_rds.tf
+++ operations/deployment/terraform/modules/aws/aurora/aws_dotenv_aurora_rds.tf
@@ -45,35 +45,35 @@ resource "local_file" "postgres-dotenv" {
 
 #### Aurora values
 # Amazon Resource Name (ARN) of cluster
-AURORA_CLUSTER_ARN=${module.rds_cluster.cluster_arn}
+POSTGRES_CLUSTER_ARN=${module.aurora_cluster.cluster_arn}
 # The RDS Cluster Identifier
-POSTGRES_CLUSTER_ID=${module.rds_cluster.cluster_id}
+POSTGRES_CLUSTER_ID=${module.aurora_cluster.cluster_id}
 # The RDS Cluster Resource ID
-POSTGRES_CLUSTER_RESOURCE_ID=${module.rds_cluster.cluster_resource_id}
+POSTGRES_CLUSTER_RESOURCE_ID=${module.aurora_cluster.cluster_resource_id}
 # Writer endpoint for the cluster
-POSTGRES_CLUSTER_ENDPOINT=${module.rds_cluster.cluster_endpoint}
+POSTGRES_CLUSTER_ENDPOINT=${module.aurora_cluster.cluster_endpoint}
 # A read-only endpoint for the cluster, automatically load-balanced across replicas
-POSTGRES_CLUSTER_READER_ENDPOINT=${module.rds_cluster.cluster_reader_endpoint}
+POSTGRES_CLUSTER_READER_ENDPOINT=${module.aurora_cluster.cluster_reader_endpoint}
 # The running version of the cluster database
-POSTGRES_CLUSTER_ENGINE_VERSION_ACTUAL=${module.rds_cluster.cluster_engine_version_actual}
+POSTGRES_CLUSTER_ENGINE_VERSION_ACTUAL=${module.aurora_cluster.cluster_engine_version_actual}
 # Name for an automatically created database on cluster creation
-# database_name is not set on `aws_rds_cluster[0]` resource if it was not specified, so can't be used in output
-POSTGRES_CLUSTER_DATABASE_NAME=${module.rds_cluster.cluster_database_name == null ? "" : module.rds_cluster.cluster_database_name}
+# database_name is not set on `aws_aurora_cluster[0]` resource if it was not specified, so can't be used in output
+POSTGRES_CLUSTER_DATABASE_NAME=${module.aurora_cluster.cluster_database_name == null ? "" : module.aurora_cluster.cluster_database_name}
 # The database port
-POSTGRES_CLUSTER_PORT="${module.rds_cluster.cluster_port}"
+POSTGRES_CLUSTER_PORT="${module.aurora_cluster.cluster_port}"
 # TODO: use IAM (give ec2 instance(s) access to the DB via a role)
 # The database master password
-POSTGRES_CLUSTER_MASTER_PASSWORD=${module.rds_cluster.cluster_master_password}
+POSTGRES_CLUSTER_MASTER_PASSWORD=${module.aurora_cluster.cluster_master_password}
 # The database master username
-POSTGRES_CLUSTER_MASTER_USERNAME=${module.rds_cluster.cluster_master_username}
+POSTGRES_CLUSTER_MASTER_USERNAME=${module.aurora_cluster.cluster_master_username}
 # The Route53 Hosted Zone ID of the endpoint
-POSTGRES_CLUSTER_HOSTED_ZONE_ID=${module.rds_cluster.cluster_hosted_zone_id}
+POSTGRES_CLUSTER_HOSTED_ZONE_ID=${module.aurora_cluster.cluster_hosted_zone_id}
 # POSTGRES specific env vars
-DBA_ENGINE="${module.rds_cluster.cluster_engine ??? } #TODO: FIX THIS
-PG_USER="${module.rds_cluster.cluster_master_username}"
-PG_PASSWORD="${module.rds_cluster.cluster_master_password}"
-PGDATABASE=${module.rds_cluster.cluster_database_name == null ? "" : module.rds_cluster.cluster_database_name}
-PGPORT=${module.rds_cluster.cluster_port}
-PGHOST="${module.rds_cluster.cluster_endpoint}"
+DBA_ENGINE="${module.aurora_cluster.cluster_engine} 
+PG_USER="${module.aurora_cluster.cluster_master_username}"
+PG_PASSWORD="${module.aurora_cluster.cluster_master_password}"
+PGDATABASE=${module.aurora_cluster.cluster_database_name == null ? "" : module.aurora_cluster.cluster_database_name}
+PGPORT=${module.aurora_cluster.cluster_port}
+PGHOST="${module.aurora_cluster.cluster_endpoint}"
 EOT
 }
