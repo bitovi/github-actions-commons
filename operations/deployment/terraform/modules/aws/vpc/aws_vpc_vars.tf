402f650f431b25191fbdf3832040afe60ccbefbe
LeoDiazL <leo@bitovi.com>
2023-07-31 16:57:19 -0300

    typo

--- operations/deployment/terraform/modules/aws/vpc/aws_vpc_vars.tf
+++ operations/deployment/terraform/modules/aws/vpc/aws_vpc_vars.tf
@@ -2,7 +2,7 @@ variable "aws_vpc_create" {}
 variable "aws_vpc_id" {}
 variable "aws_vpc_cidr_block" {}
 variable "aws_vpc_name" {}
-variable "aws_resource_identifie" {}
+variable "aws_resource_identifier" {}
 variable "aws_vpc_public_subnets" {}
 variable "aws_vpc_private_subnets" {}
 variable "aws_vpc_availability_zones" {}
