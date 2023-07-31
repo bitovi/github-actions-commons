cb90663e61c34398bde819c89509790d8b3cc33a
LeoDiazL <leo@bitovi.com>
2023-07-31 11:28:20 -0300

    Fixing typo

--- operations/deployment/terraform/aws/aws_default_vpc_subnet_sg.tf
+++ operations/deployment/terraform/aws/aws_default_vpc_subnet_sg.tf
@@ -2,9 +2,9 @@
 # Requires:
 #  - aws_ec2
 
-data "aws_vpc" "default" {
-  default = true
-}
+#data "aws_vpc" "default" {
+#  default = true
+#}
 
 data "aws_subnets" "vpc_subnets" {
   filter {
@@ -16,19 +16,19 @@ data "aws_subnets" "vpc_subnets" {
   }
 }
 
-data "aws_region" "current" {}
+#data "aws_region" "current" {}
 
 output "aws_default_subnet_ids" {
   description = "The subnet ids from the default vpc"
   value       = data.aws_subnets.vpc_subnets.ids
 }
 
-output "aws_region_current_name" {
-  description = "The AWS Current region name"
-  value       = data.aws_region.current.name
-}
+#output "aws_region_current_name" {
+#  description = "The AWS Current region name"
+#  value       = data.aws_region.current.name
+#}
 
-output "aws_security_group_default_id" {
-  description = "The AWS Default SG Id"
-  value       = data.aws_security_group.default.id
-}
+#output "aws_security_group_default_id" {
+#  description = "The AWS Default SG Id"
+#  value       = data.aws_security_group.default.id
+#}
