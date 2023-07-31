0830e380cb36acbf00982e7402d84d4070414bdb
LeoDiazL <leo@bitovi.com>
2023-07-31 12:02:50 -0300

    Forcing subnet for vpc

--- operations/deployment/terraform/aws/aws_default_azs.tf
+++ operations/deployment/terraform/aws/aws_default_azs.tf
@@ -17,31 +17,37 @@ output "aws_region_current_name" {
 data "aws_subnet" "defaulta" {
   availability_zone = "${data.aws_region.current.name}a"
   #default_for_az    = true
+  vpc_id = data.aws_vpc.default[0].id
 }
 data "aws_subnet" "defaultb" {
   count             = contains(data.aws_availability_zones.all.names, "${data.aws_region.current.name}b") ? 1 : 0
   availability_zone = "${data.aws_region.current.name}b"
   #default_for_az    = true
+  vpc_id = data.aws_vpc.default[0].id
 }
 data "aws_subnet" "defaultc" {
   count             = contains(data.aws_availability_zones.all.names, "${data.aws_region.current.name}c") ? 1 : 0
   availability_zone = "${data.aws_region.current.name}c"
   #default_for_az    = true
+  vpc_id = data.aws_vpc.default[0].id
 }
 data "aws_subnet" "defaultd" {
   count             = contains(data.aws_availability_zones.all.names, "${data.aws_region.current.name}d") ? 1 : 0
   availability_zone = "${data.aws_region.current.name}d"
   #default_for_az    = true
+  vpc_id = data.aws_vpc.default[0].id
 }
 data "aws_subnet" "defaulte" {
   count             = contains(data.aws_availability_zones.all.names, "${data.aws_region.current.name}e") ? 1 : 0
   availability_zone = "${data.aws_region.current.name}e"
   #default_for_az    = true
+  vpc_id = data.aws_vpc.default[0].id
 }
 data "aws_subnet" "defaultf" {
   count             = contains(data.aws_availability_zones.all.names, "${data.aws_region.current.name}f") ? 1 : 0
   availability_zone = "${data.aws_region.current.name}f"
   #default_for_az    = true
+  vpc_id = data.aws_vpc.default[0].id
 }
 
 data "aws_subnets" "defaults" {
