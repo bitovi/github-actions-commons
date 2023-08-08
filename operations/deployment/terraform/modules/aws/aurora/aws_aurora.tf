b532b60c3f9b8bee41dc81450b23a4ca4c24f9a8
LeoDiazL <leo@bitovi.com>
2023-08-08 10:00:16 -0300

    Adding aurora db lifecycle tag

--- operations/deployment/terraform/modules/aws/aurora/aws_aurora.tf
+++ operations/deployment/terraform/modules/aws/aurora/aws_aurora.tf
@@ -105,6 +105,10 @@ module "aurora_cluster" {
   tags = {
     Name = "${var.aws_resource_identifier} - Aurora"
   }
+  lifecycle {
+    # Reference the security group as a whole or individual attributes like `name`
+    replace_triggered_by = [aws_security_group.aurora_security_group]
+  }
 }
 
 resource "random_password" "rds" {
