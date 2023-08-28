4e59b3b959749e1a17651bf88e44de0b2276c9d0
LeoDiazL <leo@bitovi.com>
2023-08-28 13:37:04 -0300

    Provider def'n

--- operations/deployment/terraform/modules/aws/ec2/aws_ec2.tf
+++ operations/deployment/terraform/modules/aws/ec2/aws_ec2.tf
@@ -136,3 +136,9 @@ output "aws_instance_server_id" {
 output "private_key_filename" {
   value = local_sensitive_file.private_key.filename
 }
+
+provider "aws" {
+  default_tags {
+    tags = var.default_tags
+  }
+}
