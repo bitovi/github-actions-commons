425e2e8bddb8ea37b026e182890ce3c2be380591
LeoDiazL <leo@bitovi.com>
2023-08-28 14:53:02 -0300

    Proverds v9

---
+++ operations/deployment/terraform/modules/aws/ec2/aws_ec2_provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  default_tags {
+    tags = local.default_tags
+  }
+}
