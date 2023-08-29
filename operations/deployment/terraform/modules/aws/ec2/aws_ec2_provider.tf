cfda723d55eb521d15758648443c8f99cc3ec737
LeoDiazL <leo@bitovi.com>
2023-08-29 09:56:08 -0300

    Providers 2/9

--- operations/deployment/terraform/modules/aws/ec2/aws_ec2_provider.tf
+++ operations/deployment/terraform/modules/aws/ec2/aws_ec2_provider.tf
@@ -1,9 +1,9 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-      configuration_aliases = [ aws.ec2 ]
-    }
-  }
-}
+#terraform {
+#  required_providers {
+#    aws = {
+#      source  = "hashicorp/aws"
+#      version = "~> 5.0"
+#      configuration_aliases = [ aws.ec2 ]
+#    }
+#  }
+#}
