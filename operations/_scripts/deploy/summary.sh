d3f9aebacaddc3cc399524748d0dfef007325cad
LeoDiazL <leo@bitovi.com>
2023-09-06 17:41:47 -0300

    Fixing summary v2

--- operations/_scripts/deploy/summary.sh
+++ operations/_scripts/deploy/summary.sh
@@ -106,7 +106,7 @@ fi
 
 echo -e "$result_string" >> $GITHUB_STEP_SUMMARY
 if [[ $SUCCESS == 'success' ]]; then
-  if [[ $URL_OUTPUT != '' ]]; then
+  if [[ $URL_OUTPUT != '' ]] || [[ $EC2_URL_OUTPUT != '' ]]; then
     while IFS= read -r line; do
       echo -e "$line" >> $GITHUB_STEP_SUMMARY
     done <<< "$final_output"
