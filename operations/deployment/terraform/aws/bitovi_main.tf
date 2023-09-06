800403f8d4bde3f7220ba1d122e11516c7257c80
LeoDiazL <leo@bitovi.com>
2023-09-06 17:19:19 -0300

    Fixing elb null

--- operations/deployment/terraform/aws/bitovi_main.tf
+++ operations/deployment/terraform/aws/bitovi_main.tf
@@ -271,7 +271,7 @@ locals {
   ec2_public_endpoint  = module.ec2[0].instance_public_dns != null ? module.ec2[0].instance_public_dns : module.ec2[0].instance_public_ip
   ec2_private_endpoint = module.ec2[0].instance_private_dns != null ? module.ec2[0].instance_private_dns : module.ec2[0].instance_private_ip
   ec2_endpoint         = local.ec2_public_endpoint != null ? local.ec2_public_endpoint : local.ec2_private_endpoint
-  ec2_no_dns_url       = module.aws_elb[0].aws_elb_dns_name != null ? module.aws_elb[0].aws_elb_dns_name : local.ec2_endpoint
+  ec2_no_dns_url       = try(module.aws_elb[0].aws_elb_dns_name,local.ec2_endpoint)
   ec2_no_dns_url_fqdn  = local.ec2_no_dns_url != null ? "http://${local.ec2_no_dns_url}" : null
 }
 
