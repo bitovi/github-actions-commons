8b5207456499bc02e71f3792c721d694e27051d0
LeoDiazL <leo@bitovi.com>
2023-09-13 14:04:18 -0300

    Changing targets

--- operations/_scripts/generate/generate_bitops_config.sh
+++ operations/_scripts/generate/generate_bitops_config.sh
@@ -80,8 +80,7 @@ fi
 if ([[ $(alpha_only "$AWS_EC2_INSTANCE_CREATE") == true ]] || [[ $(alpha_only "$AWS_EFS_ENABLE") == true ]] || [[ $(alpha_only "$AWS_AURORA_ENABLE") == true ]]) && [[ "$(alpha_only $TF_STACK_DESTROY)" != "true" ]]; then
   # random_integer.az_select needs to be created before the "full stack" to avoid a potential state dependency locks
   targets="$targets
-      - module.vpc"
-     # - module.vpc.random_integer.az_select"
+      - module.vpc.random_integer.az_select"
   # In the case VPC creation is enabled, as it's a needed resource for the whole stack, will trigger creation first.
   if [[ $(alpha_only "$AWS_VPC_CREATE") == true ]]; then
   targets="$targets
