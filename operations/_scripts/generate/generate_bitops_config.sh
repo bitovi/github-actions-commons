5636b9d86297432706dcbf61a189f147678dfbeb
LeoDiazL <leo@bitovi.com>
2023-09-18 17:53:06 -0300

    Adding more targets

--- operations/_scripts/generate/generate_bitops_config.sh
+++ operations/_scripts/generate/generate_bitops_config.sh
@@ -83,6 +83,8 @@ if ([[ $(alpha_only "$AWS_EC2_INSTANCE_CREATE") == true ]] || [[ $(alpha_only "$
       - module.vpc.local.use_default
       - module.vpc.local.preferred_az
       - module.vpc.aws_availability_zones.all
+      - module.vpc.aws_ec2_instance_type_offerings.region_azs
+      - module.vpc.aws_subnet.selected[0].availability_zone
       - module.vpc.random_integer.az_select"
       #- module.vpc.aws_ec2_instance_type_offerings.region_azs
   # In the case VPC creation is enabled, as it's a needed resource for the whole stack, will trigger creation first.
