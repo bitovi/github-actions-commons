d350d6e289366bf6a293f2ae2368c43b45a5e81f
LeoDiazL <leo@bitovi.com>
2023-09-18 18:14:15 -0300

    cleanup

--- operations/_scripts/generate/generate_bitops_config.sh
+++ operations/_scripts/generate/generate_bitops_config.sh
@@ -80,12 +80,12 @@ fi
 if ([[ $(alpha_only "$AWS_EC2_INSTANCE_CREATE") == true ]] || [[ $(alpha_only "$AWS_EFS_ENABLE") == true ]] || [[ $(alpha_only "$AWS_AURORA_ENABLE") == true ]]) && [[ "$(alpha_only $TF_STACK_DESTROY)" != "true" ]]; then
   # random_integer.az_select needs to be created before the "full stack" to avoid a potential state dependency locks
   targets="$targets
-      - module.vpc.local.use_default
-      - module.vpc.local.preferred_az
-      - module.vpc.aws_availability_zones.all
-      - module.vpc.aws_ec2_instance_type_offerings.region_azs
-      - module.vpc.aws_subnet.selected[0].availability_zone
       - module.vpc.random_integer.az_select"
+     # - module.vpc.local.use_default
+     # - module.vpc.local.preferred_az
+     # - module.vpc.aws_availability_zones.all
+     # - module.vpc.aws_ec2_instance_type_offerings.region_azs
+     # - module.vpc.aws_subnet.selected[0].availability_zone
       #- module.vpc.aws_ec2_instance_type_offerings.region_azs
   # In the case VPC creation is enabled, as it's a needed resource for the whole stack, will trigger creation first.
   if [[ $(alpha_only "$AWS_VPC_CREATE") == true ]]; then
