9d9bca9b745779db73d132604be6038a2541823f
LeoDiazL <leo@bitovi.com>
2023-10-03 10:02:31 -0300

    Adding testing role arn

--- operations/deployment/terraform/modules/aws/ecs/aws_ecs.tf
+++ operations/deployment/terraform/modules/aws/ecs/aws_ecs.tf
@@ -11,7 +11,7 @@ resource "aws_ecs_task_definition" "ecs_task" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = tonumber(var.aws_ecs_app_cpu)
   memory                   = tonumber(var.aws_ecs_app_mem)
-
+  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
   container_definitions = <<DEFINITION
 [
   {
@@ -50,6 +50,27 @@ resource "aws_ecs_service" "ecs_service_with_lb" {
     container_port   = var.aws_ecs_container_port
   }
   depends_on = [aws_alb_listener.lb_listener]
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = aws_iam_role.ecsTaskExecutionRole.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 #resource "aws_ecs_service" "ecs_service_no_lb" {
