6b55d53f414a19be7f1cb3e118ae50298baff4be
LeoDiazL <leo@bitovi.com>
2023-10-05 16:01:15 -0300

    Fixing role

--- operations/deployment/terraform/modules/aws/ecs/aws_ecs.tf
+++ operations/deployment/terraform/modules/aws/ecs/aws_ecs.tf
@@ -125,26 +125,30 @@ resource "aws_cloudwatch_log_group" "ecs_cw_log_group" {
 
 # IAM
 
-resource "aws_iam_role" "ecsTaskExecutionRole" {
-  name               = "${var.aws_resource_identifier}-Role"
-  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+data "aws_iam_role" "ecsTaskExecutionRole" {
+      name = "ecsTaskExecutionRole"
 }
 
-data "aws_iam_policy_document" "assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
-  role       = aws_iam_role.ecsTaskExecutionRole.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
+#resource "aws_iam_role" "ecsTaskExecutionRole" {
+#  name               = "${var.aws_resource_identifier}-Role"
+#  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+#}
+#
+#data "aws_iam_policy_document" "assume_role_policy" {
+#  statement {
+#    actions = ["sts:AssumeRole"]
+#
+#    principals {
+#      type        = "Service"
+#      identifiers = ["ecs-tasks.amazonaws.com"]
+#    }
+#  }
+#}
+#
+#resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+#  role       = aws_iam_role.ecsTaskExecutionRole.name
+#  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+#}
 
 #resource "aws_ecs_service" "ecs_service_no_lb" {
 #  name             = var.aws_ecs_service_name != "" ? var.aws_ecs_service_name : "${var.aws_resource_identifier}-service"
