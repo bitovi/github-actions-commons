70c66bf16c21a8ebe45e2e39f29ce739526007cb
LeoDiazL <leo@bitovi.com>
2023-10-05 17:56:05 -0300

    Cleanup and bump

---
+++ operations/deployment/terraform/modules/aws/ecs/aws_ecs_networking.tf
@@ -0,0 +1,109 @@
+locals {
+  aws_ecs_container_port = var.aws_ecs_container_port# != "" ? [for n in split(",", var.aws_ecs_container_port) : tonumber(n)] : []
+  aws_ecs_lb_port        = var.aws_ecs_lb_port != "" ?  var.aws_ecs_lb_port : local.aws_ecs_container_port #      [for n in split(",", var.aws_ecs_lb_port)        : tonumber(n)] : local.aws_ecs_container_port
+}
+
+# Network part
+resource "aws_security_group" "ecs_sg" {
+  name        = var.aws_ecs_security_group_name != "" ? var.aws_ecs_security_group_name : "SG for ${var.aws_resource_identifier} ECS"
+  description = "SG for ${var.aws_resource_identifier} - ${local.aws_ecs_task_name} - ECS"
+  vpc_id      = var.aws_selected_vpc_id
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "${var.aws_resource_identifier}-ecs-sg"
+  }
+}
+
+#resource "aws_security_group_rule" "incoming_ecs_ports" {
+# # count             = length(local.aws_ecs_container_port)
+#  type              = "ingress"
+#  from_port         = local.aws_ecs_container_port#[count.index]
+#  to_port           = local.aws_ecs_container_port#[count.index]
+#  protocol          = "tcp"
+#  cidr_blocks       = ["0.0.0.0/0"]
+#  security_group_id = aws_security_group.ecs_sg.id
+#}
+
+resource "aws_security_group_rule" "incoming_alb" {
+  #count                    = length(local.aws_ecs_container_port)
+  type                     = "ingress"
+  from_port                = local.aws_ecs_container_port#[count.index]
+  to_port                  = local.aws_ecs_container_port#[count.index]
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.ecs_lb_sg.id
+  security_group_id        = aws_security_group.ecs_sg.id
+}
+
+### ALB --- Make this optional -- Using ALB name intentionally. (To make clear is an A LB)
+
+resource "aws_alb" "ecs_lb" {
+  name            = var.aws_resource_identifier_supershort
+  subnets         = var.aws_selected_subnets
+  security_groups = [aws_security_group.ecs_lb_sg.id]
+
+  tags = {
+    Name = "${var.aws_resource_identifier_supershort}"
+  }
+}
+
+resource "aws_alb_target_group" "lb_targets" {
+  #count       = length(local.aws_ecs_container_port)
+  name        = "${var.aws_resource_identifier_supershort}"
+  port        = local.aws_ecs_container_port#[count.index]
+  protocol    = "HTTP"
+  vpc_id      = var.aws_selected_vpc_id
+  target_type = "ip"
+
+  lifecycle {
+    replace_triggered_by = [aws_security_group.ecs_sg]
+  }
+}
+
+# Redirect all traffic from the ALB to the target group
+resource "aws_alb_listener" "lb_listener" {
+  #count             = length(local.aws_ecs_lb_port)
+  load_balancer_arn = "${aws_alb.ecs_lb.id}"
+  port              = local.aws_ecs_lb_port#[count.index]
+  protocol          = "HTTP"
+
+  default_action {
+#    target_group_arn = aws_alb_target_group.lb_targets[count.index].id
+    target_group_arn = aws_alb_target_group.lb_targets.id
+    type             = "forward"
+  }
+}
+
+resource "aws_security_group" "ecs_lb_sg" {
+  name        = var.aws_ecs_security_group_name != "" ? "${var.aws_ecs_security_group_name}-lb" : "SG for ${var.aws_resource_identifier} ECS LB"
+  description = "SG for ${var.aws_resource_identifier} - ${local.aws_ecs_task_name} ECS Load Balancer"
+  vpc_id      = var.aws_selected_vpc_id
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "${var.aws_resource_identifier}-ecs-lb-sg"
+  }
+}
+
+resource "aws_security_group_rule" "incoming_ecs_lb_ports" {
+  #count       = length(local.aws_ecs_lb_port)
+  type        = "ingress"
+  from_port   = local.aws_ecs_lb_port#[count.index]
+  to_port     = local.aws_ecs_container_port#[count.index]
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ecs_lb_sg.id
+}
+
+output "load_balancer_dns" {
+  value = aws_alb.ecs_lb.dns_name
+}
