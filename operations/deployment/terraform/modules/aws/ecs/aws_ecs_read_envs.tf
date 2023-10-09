4b4cdb284e25aa689872ecc9e40b0e7d6291d0cd
LeoDiazL <leo@bitovi.com>
2023-10-09 17:41:19 -0300

    Testing with file_list

--- operations/deployment/terraform/modules/aws/ecs/aws_ecs_read_envs.tf
+++ operations/deployment/terraform/modules/aws/ecs/aws_ecs_read_envs.tf
@@ -2,15 +2,23 @@ locals {
   env_repo_file = format("%s/%s", abspath(path.root), "../env-files/repo.env")
   env_ghv_file  = format("%s/%s", abspath(path.root), "../env-files/ghv.env")
   env_ghs_file  = format("%s/%s", abspath(path.root), "../env-files/ghs.env")
-  env_aws_file  = format("%s/%s", abspath(path.root), "../env-files/aws.env")  
+  env_aws_file  = format("%s/%s", abspath(path.root), "../env-files/aws.env")
+  file_list     = compact([fileexists(local.env_repo_file) ? local.env_aws_file : null, 
+                           fileexists(local.env_ghv_file) ? local.env_ghv_file : null,
+                           fileexists(local.env_ghs_file) ? local.env_ghs_file : null,
+                           fileexists(local.env_aws_file) ? local.env_aws_file : null])
+}
+
+output "file_list" {
+    value = local.file_list
 }
 
 data "template_file" "variable_template" {
-  count = can(file(local.env_repo_file)) ? 1 : 0
+  count = length(local.file_list)
 
   template = <<EOF
 [
-  % for pair in local.env_repo_file :
+  % for pair in local.file_list[count.index] :
   {
     "name": "${pair[0]}",
     "value": "${pair[1]}"
@@ -21,7 +29,7 @@ EOF
 
   vars = {
     variable_map = tomap(
-      flatten([for line in file("${local.env_repo_file}") : split("=", line)])
+      flatten([for line in file("${local.file_list[count.index]}") : split("=", line)])
     )
   }
 }
