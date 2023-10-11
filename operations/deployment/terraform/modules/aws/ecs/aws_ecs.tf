d5f08b1d2a751621cf09acf97b044720fe2ea6b6
LeoDiazL <leo@bitovi.com>
2023-10-11 12:12:38 -0300

    Reading file directly

--- operations/deployment/terraform/modules/aws/ecs/aws_ecs.tf
+++ operations/deployment/terraform/modules/aws/ecs/aws_ecs.tf
@@ -78,7 +78,9 @@ resource "aws_ecs_task_definition" "ecs_task_from_json" {
   cpu                      = local.aws_ecs_app_cpu[count.index]
   memory                   = local.aws_ecs_app_mem[count.index]
   execution_role_arn       = data.aws_iam_role.ecsTaskExecutionRole.arn
-  container_definitions    = sensitive(file(format("%s/%s", abspath(path.root), "../../ansible/clone_repo/${var.app_repo_name}/${local.aws_ecs_task_json_definition_file[count.index]}")))
+  container_definitions    = sensitive(file("../../ansible/clone_repo/${var.app_repo_name}/${local.aws_ecs_task_json_definition_file[count.index]}"))
+  #  container_definitions    = sensitive(file(format("%s/%s", abspath(path.root), "../../ansible/clone_repo/${var.app_repo_name}/${local.aws_ecs_task_json_definition_file[count.index]}")))
+
 }
 
 #data "template_file" "container_definition" {
