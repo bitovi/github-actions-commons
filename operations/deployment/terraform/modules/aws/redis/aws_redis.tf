8bdfc1b41a023df206c3cfc0f4ac9169dce06c47
LeoDiazL <leo@bitovi.com>
2023-11-03 10:41:43 -0300

    Fix

--- operations/deployment/terraform/modules/aws/redis/aws_redis.tf
+++ operations/deployment/terraform/modules/aws/redis/aws_redis.tf
@@ -55,7 +55,7 @@ resource "aws_elasticache_subnet_group" "selected" {
 #########
 
 resource "aws_elasticache_replication_group" "redis_cluster" {
-  automatic_failover_enabled  = tonumber(var.aws_redis_num_cache_clusters) > 1 ? true : contains(var.aws_redis_parameter_group_name, "cluster")
+  automatic_failover_enabled  = tonumber(var.aws_redis_num_cache_clusters) > 1 ? true : strcontains(var.aws_redis_parameter_group_name, "cluster")
   replication_group_id        = var.aws_redis_replication_group_id != "" ? var.aws_redis_replication_group_id : "${var.aws_resource_identifier_supershort}-redis"
   description                 = "Redis cluster for ${var.aws_resource_identifier}" 
   node_type                   = var.aws_redis_node_type
