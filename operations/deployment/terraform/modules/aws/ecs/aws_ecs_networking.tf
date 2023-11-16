fa33b2cd7736796b1eea162075442c96401ce974
LeoDiazL <leo@bitovi.com>
2023-11-16 13:16:33 -0300

    how about this dyn block?

--- operations/deployment/terraform/modules/aws/ecs/aws_ecs_networking.tf
+++ operations/deployment/terraform/modules/aws/ecs/aws_ecs_networking.tf
@@ -63,8 +63,8 @@ resource "aws_alb_listener" "lb_listener" {
   count             = length(local.aws_ecs_lb_port)
   load_balancer_arn = "${aws_alb.ecs_lb.id}"
   port              = local.aws_ecs_lb_port[count.index]
-  ssl_policy        = var.aws_certificate_enabled ? "ELBSecurityPolicy-TLS13-1-2-2021-06" : ""
-  protocol          = var.aws_certificate_enabled ?  "HTTPS" : "HTTP"
+  #ssl_policy        = var.aws_certificate_enabled ? "ELBSecurityPolicy-TLS13-1-2-2021-06" : ""
+  #protocol          = var.aws_certificate_enabled ?  "HTTPS" : "HTTP"
   certificate_arn   = var.aws_certificates_selected_arn
   #protocol          = var.aws_certificates_selected_arn != "" ? "HTTPS" : "HTTP"
   #ssl_policy        = var.aws_certificates_selected_arn != "" ? "ELBSecurityPolicy-TLS13-1-2-2021-06" : "" # https://docs.aws.amazon.com/elasticloadbalancing/latest/application/create-https-listener.html
@@ -74,6 +74,21 @@ resource "aws_alb_listener" "lb_listener" {
   }
   lifecycle {
     replace_triggered_by = [ aws_alb_listener.http_redirect ]
+  }
+  dynamic "ssl_on" {
+    for_each = var.aws_certificate_enabled ? [1] : []
+    content {
+      protocol = "HTTPS"
+      ssl_policy = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+    }
+  }
+
+  dynamic "ssl_off" {
+    for_each = var.aws_certificate_enabled ? [] : [1]
+    content {
+      protocol = "HTTP"
+      ssl_policy = ""
+    }
   }
 }
 
