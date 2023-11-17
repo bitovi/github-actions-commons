3007aec624658699f83566e0019bfbbefac38944
LeoDiazL <116020815+LeoDiazL@users.noreply.github.com>
2023-11-17 16:23:07 -0300

    Adding cert to RDS - Send proxy output to .env (#60)
    * Fix readme and cloudwatch+docker dep
    
    * Initial commit
    
    * Changing defaults
    
    * Adding proxy.env content and fixing redis.env merge
    
    * missing index
    
    * small typo

--- operations/deployment/terraform/modules/aws/db_proxy/aws_dotenv_proxy.tf
+++ operations/deployment/terraform/modules/aws/db_proxy/aws_dotenv_proxy.tf
@@ -5,7 +5,7 @@ locals {
   file_name = var.aws_rds_db_proxy ? "rds" : var.aws_aurora_proxy ? "aurora" : "proxy"
   dot_env   = <<-EOT
 #### Proxy values for ${local.file_name}
-${local.var_name}=${aws_db_proxy.rds_proxy[0].endpoint}"
+${local.var_name}="${aws_db_proxy.rds_proxy[0].endpoint}"
 EOT
 }
 
