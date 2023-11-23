56d96571730d62750e5a0e32c473f88bc1b95e91
LeoDiazL <leo@bitovi.com>
2023-11-23 10:25:46 -0300

    cleanup 2

--- operations/deployment/terraform/modules/aws/aurora/aws_aurora.tf
+++ operations/deployment/terraform/modules/aws/aurora/aws_aurora.tf
@@ -114,9 +114,6 @@ resource "aws_rds_cluster" "aurora" {
       availability_zones
     ]
   }
-
-  #  depends_on = [ time_sleep.wait_before_deletion ]
-
 }
 
 resource "aws_rds_cluster_instance" "cluster_instance" {
