abb4b07d33b8e2529dd5319ae211d5433d85d890
LeoDiazL <leo@bitovi.com>
2023-12-15 15:57:49 -0300

    Replacing auto-scaling group

--- operations/deployment/terraform/modules/aws/eks/aws_eks_cluster.tf
+++ operations/deployment/terraform/modules/aws/eks/aws_eks_cluster.tf
@@ -115,29 +115,53 @@ locals {
   USERDATA
 }
 
-resource "aws_autoscaling_group" "main" {
-  desired_capacity     = var.aws_eks_desired_capacity
+#resource "aws_autoscaling_group" "main" {
+#  desired_capacity     = var.aws_eks_desired_capacity
+#  launch_template {
+#    id      = aws_launch_template.main.id
+#    version = "${aws_launch_template.main.latest_version}"
+#  }
+#  max_size             = var.aws_eks_max_size
+#  min_size             = var.aws_eks_min_size
+#  name                 = "${var.aws_resource_identifier}-${var.aws_eks_environment}-eksworker-asg"
+#  vpc_zone_identifier  = data.aws_subnets.private.ids
+#  health_check_type    = "EC2"
+#
+#tag {
+#  key                 = "Name"
+#  value               = "${var.aws_resource_identifier}-${var.aws_eks_environment}-eksworker-node"
+#  propagate_at_launch = true
+#}
+#
+#  depends_on = [
+#    aws_iam_role.iam_role_master,
+#    aws_iam_role.iam_role_worker,
+#    aws_security_group.eks_security_group_master,
+#    aws_security_group.eks_security_group_worker
+#  ]
+#}
+
+resource "aws_eks_node_group" "worker_nodes" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "worker-node-group"
+  subnet_ids      = data.aws_subnets.private.ids
+  node_role_arn   = aws_iam_role.iam_role_worker.arn
+
+  scaling_config {
+    desired_size = var.aws_eks_desired_capacity
+    max_size     = var.aws_eks_max_size
+    min_size     = var.aws_eks_min_size
+  }
+
   launch_template {
     id      = aws_launch_template.main.id
     version = "${aws_launch_template.main.latest_version}"
   }
-  max_size             = var.aws_eks_max_size
-  min_size             = var.aws_eks_min_size
-  name                 = "${var.aws_resource_identifier}-${var.aws_eks_environment}-eksworker-asg"
-  vpc_zone_identifier  = data.aws_subnets.private.ids
-  health_check_type    = "EC2"
-
-tag {
-  key                 = "Name"
-  value               = "${var.aws_resource_identifier}-${var.aws_eks_environment}-eksworker-node"
-  propagate_at_launch = true
-}
 
   depends_on = [
-    aws_iam_role.iam_role_master,
+    aws_eks_cluster.main,
     aws_iam_role.iam_role_worker,
-    aws_security_group.eks_security_group_master,
-    aws_security_group.eks_security_group_worker
+    aws_launch_template.main,
   ]
 }
 
