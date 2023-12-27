f32ab161d3b8a2ff2f4547ff43e4aed1f4c4591b
LeoDiazL <leo@bitovi.com>
2023-12-27 11:11:29 -0300

    Fixing output

--- operations/deployment/terraform/modules/aws/eks/aws_eks_auth.tf
+++ operations/deployment/terraform/modules/aws/eks/aws_eks_auth.tf
@@ -56,3 +56,13 @@ ROLES
   }
   depends_on = [ aws_eks_cluster.main ]
 }
+
+
+
+output "eks_kubernetes_provider_config" {
+  value = {
+    host                   = data.aws_eks_cluster.eks_cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster_auth.token
+  }
+}
