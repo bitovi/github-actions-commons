49e48e8fc9a0a40d17e9d61cf54a77ea071da71d
LeoDiazL <leo@bitovi.com>
2023-12-27 11:07:46 -0300

    Failing it

--- operations/deployment/terraform/aws/provider.tf
+++ operations/deployment/terraform/aws/provider.tf
@@ -1,6 +1,6 @@
 provider "kubernetes" {
   alias                  = "eks"
-  host                   = try(module.eks[0].eks_kubernetes_provider_config["host"],null)
-  cluster_ca_certificate = try(module.eks[0].eks_kubernetes_provider_config["cluster_ca_certificate"],null)
-  token                  = try(module.eks[0].eks_kubernetes_provider_config["token"],null)
+  host                   = module.eks[0].eks_kubernetes_provider_config["host"]
+  cluster_ca_certificate = module.eks[0].eks_kubernetes_provider_config["cluster_ca_certificate"]
+  token                  = module.eks[0].eks_kubernetes_provider_config["token"]
 }
