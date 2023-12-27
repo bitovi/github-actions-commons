be95d582f3566addc38ec36f339060f298b81b71
LeoDiazL <leo@bitovi.com>
2023-12-27 17:10:45 -0300

    Remove dupe

--- operations/deployment/terraform/aws/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "kubernetes" {
-  alias                  = "eks"
-  host                   = module.eks[0].eks_kubernetes_provider_config["host"]
-  cluster_ca_certificate = module.eks[0].eks_kubernetes_provider_config["cluster_ca_certificate"]
-  token                  = module.eks[0].eks_kubernetes_provider_config["token"]
-}
