6f9c6e10a7338b1680bbba1a06280748c89f41c1
LeoDiazL <leo@bitovi.com>
2023-12-27 10:50:20 -0300

    Fixing prov

--- operations/deployment/terraform/modules/aws/eks/aws_eks_providers.tf
+++ operations/deployment/terraform/modules/aws/eks/aws_eks_providers.tf
@@ -11,8 +11,8 @@ terraform {
   }
 }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks_cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster_auth.token
-}
+#provider "kubernetes" {
+#  host                   = data.aws_eks_cluster.eks_cluster.endpoint
+#  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority.0.data)
+#  token                  = data.aws_eks_cluster_auth.cluster_auth.token
+#}
