554db5aa123636deabe887cd0615c0adcdbe8332
LeoDiazL <leo@bitovi.com>
2024-01-03 15:38:10 -0300

    Cleanup v2

--- operations/deployment/terraform/modules/aws/vpc/aws_vpc_azs.tf
+++ operations/deployment/terraform/modules/aws/vpc/aws_vpc_azs.tf
@@ -87,10 +87,6 @@ data "aws_subnet" "selected" {
   id    = var.aws_vpc_subnet_id != "" ? var.aws_vpc_subnet_id : var.aws_vpc_create ? aws_subnet.public[0].id : data.aws_subnets.vpc_subnets.ids[0]
 }
 
-output "aws_vpc_subnet_selected" {
-  value = local.use_default ? data.aws_subnet.default_selected[0].id : data.aws_subnet.selected[0].id
-}
-
 data "aws_security_group" "default" {
   filter {
     name   = "group-name"
