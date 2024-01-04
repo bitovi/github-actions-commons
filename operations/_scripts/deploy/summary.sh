1ed0cabca27cecc84f9cfe68bebfbda39d66b57c
LeoDiazL <leo@bitovi.com>
2024-01-04 15:06:52 -0300

    Removing debug

--- operations/_scripts/deploy/summary.sh
+++ operations/_scripts/deploy/summary.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # shellcheck disable=SC2086
 
-set -x
-
 ### coming into this we have env vars:
 # SUCCESS=${{ job.status }} # success, cancelled, failure
 # URL_OUTPUT=${{ steps.deploy.outputs.vm_url }}
