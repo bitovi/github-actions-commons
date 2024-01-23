5e14a722006b0649f9b6dfdd381509e6d0bd7176
LeoDiazL <116020815+LeoDiazL@users.noreply.github.com>
2024-01-23 14:34:47 -0300

    Fixing the message to something more meaningfull

--- operations/deployment/terraform/ecr/bitops.before-deploy.d/create-tf-state-bucket.sh
+++ operations/deployment/terraform/ecr/bitops.before-deploy.d/create-tf-state-bucket.sh
@@ -11,14 +11,14 @@ else
   else
     aws s3api create-bucket --bucket $TF_STATE_BUCKET --region $AWS_DEFAULT_REGION --create-bucket-configuration LocationConstraint=$AWS_DEFAULT_REGION || true
   fi
-  touch bitovi-test-file.txt
-  if ! [[ -z $(aws s3api put-object --bucket $TF_STATE_BUCKET --key bitovi-test-file.txt --body ./bitovi-test-file.txt 2>&1 >/dev/null ) ]]; then
+  echo "File only needed to test bucket access. Shouldn't be here. Can be safely deleted" >> bitovi-github-actions-commons.statecheck.txt
+  if ! [[ -z $(aws s3api put-object --bucket $TF_STATE_BUCKET --key bitovi-github-actions-commons.statecheck.txt --body ./bitovi-github-actions-commons.statecheck.txt 2>&1 >/dev/null ) ]]; then
     echo "Permission issue: Unable to write to the bucket."
     exit 63
   else
-      if ! [[ $(aws s3 rm "s3://$TF_STATE_BUCKET/bitovi-test-file.txt" 2>&1 >/dev/null ) ]]; then
+      if ! [[ $(aws s3 rm "s3://$TF_STATE_BUCKET/bitovi-github-actions-commons.statecheck.txt" 2>&1 >/dev/null ) ]]; then
             echo "Access to bucket confirmed. Can create and delete a tf-state file"
       fi
   fi
-  rm bitovi-test-file.txt
+  rm bitovi-github-actions-commons.statecheck.txt
 fi
