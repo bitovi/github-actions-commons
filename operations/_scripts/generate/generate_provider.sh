7c6952bb49251f26b386c417043880b43742b287
LeoDiazL <116020815+LeoDiazL@users.noreply.github.com>
2025-01-27 09:44:53 -0300

    Adding missing ecr

--- operations/_scripts/generate/generate_provider.sh
+++ operations/_scripts/generate/generate_provider.sh
@@ -74,7 +74,7 @@ provider \"kubernetes\" {
 }" >> "${GITHUB_ACTION_PATH}/operations/deployment/terraform/$1/bitovi_provider.tf"
 }
 
-generate_provider_aws aws ec2,r53,elb,efs,vpc,rds,aurora,ecs,db_proxy,redis,eks # Added eks here
+generate_provider_aws aws ec2,r53,elb,efs,vpc,rds,aurora,ecs,db_proxy,redis,eks,ecr # Added eks here
 #generate_provider_aws ecr ecr
 #generate_provider_aws eks
 
