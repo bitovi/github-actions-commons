fa73d383683a1953521514107558c58583afe2fa
Mick McGrath <mickmcgrath13@users.noreply.github.com>
2025-03-03 16:04:59 +0100

    use rsync for remaining chart folders

--- operations/_scripts/generate/generate_bitops_incoming.sh
+++ operations/_scripts/generate/generate_bitops_incoming.sh
@@ -205,8 +205,8 @@ function helm_move_content_prepend() {
     done
     # Move remaining folders (if they exist) and exclude the . folder
     find "$chart_folder" -maxdepth 1 -type d -not -name "." -path "$chart_folder/*" | while read folder; do
-      echo "mv $folder $destination_folder/$chart_name/."
-      mv "$folder" "$destination_folder/$chart_name/."
+      echo "rsync -av $folder $destination_folder/$chart_name/"
+      rsync -av "$folder" "$destination_folder/$chart_name/"
     done
     echo "Printing chart result"
     tree "$destination_folder/$chart_name"
