6d8d77a263ac0a050d5b69a32cd261b919a3d39b
LeoDiazL <leo@bitovi.com>
2025-08-27 18:14:12 -0300

    Typo

--- operations/deployment/terraform/modules/aws/waf/aws_waf.tf
+++ operations/deployment/terraform/modules/aws/waf/aws_waf.tf
@@ -375,10 +375,6 @@ resource "aws_wafv2_web_acl_association" "waf_association" {
   resource_arn = var.aws_lb_resource_arn
   web_acl_arn  = aws_wafv2_web_acl.waf[0].arn
 }
-resource "aws_wafv2_web_acl_logging_configuration" "example" {
-  log_destination_configs = [aws_cloudwatch_log_group.example.arn]
-  resource_arn            = aws_wafv2_web_acl.example.arn
-}
 
 # WAF Logging Configuration (optional)
 resource "aws_wafv2_web_acl_logging_configuration" "waf_logging" {
