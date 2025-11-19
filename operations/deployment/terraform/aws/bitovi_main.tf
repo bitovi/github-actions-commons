d79b1934a8dcba928e7fc4efd168fc82f65f5574
LeoDiazL <leo@bitovi.com>
2025-11-19 20:31:11 -0300

    fix-make-ecs-apex-rule-root-domain-dependant

--- operations/deployment/terraform/aws/bitovi_main.tf
+++ operations/deployment/terraform/aws/bitovi_main.tf
@@ -501,7 +501,7 @@ module "aws_ecs" {
   aws_ecs_lb_redirect_enable         = var.aws_ecs_lb_redirect_enable
   aws_ecs_lb_container_path          = var.aws_ecs_lb_container_path
   aws_ecs_lb_ssl_policy              = var.aws_ecs_lb_ssl_policy
-  aws_ecs_lb_www_to_apex_redirect    = var.aws_ecs_lb_www_to_apex_redirect
+  aws_ecs_lb_www_to_apex_redirect    = var.aws_r53_root_domain_deploy ? var.aws_ecs_lb_www_to_apex_redirect : false
   aws_ecs_autoscaling_enable         = var.aws_ecs_autoscaling_enable
   aws_ecs_autoscaling_max_nodes      = var.aws_ecs_autoscaling_max_nodes
   aws_ecs_autoscaling_min_nodes      = var.aws_ecs_autoscaling_min_nodes
