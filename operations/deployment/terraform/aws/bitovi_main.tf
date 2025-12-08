fc50d2b98c39f4fe1510069cc86a6313b55cd349
LeoDiazL <leo@bitovi.com>
2025-12-08 17:48:40 -0300

    Fix main conditional

--- operations/deployment/terraform/aws/bitovi_main.tf
+++ operations/deployment/terraform/aws/bitovi_main.tf
@@ -88,7 +88,7 @@ module "aws_route53" {
   aws_r53_enable_cert        = var.aws_r53_enable_cert
   # ELB
   aws_elb_dns_name = try(module.aws_lb[0].aws_alb_dns_name,module.aws_elb[0].aws_elb_dns_name,module.ec2[0].instance_public_ip,"")
-  aws_elb_zone_id  = try(module.aws_lb[0].aws_alb_zone_id,module.aws_elb[0].aws_elb_zone_id,null,"")
+  aws_elb_zone_id  = try(module.aws_lb[0].aws_alb_zone_id,module.aws_elb[0].aws_elb_zone_id,"","")
   # Certs
   aws_certificates_selected_arn = var.aws_r53_enable_cert && var.aws_r53_domain_name != "" ? module.aws_certificates[0].selected_arn : ""
   # Others
