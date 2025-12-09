6853afd1ec091261fc6e9d620f0bb9d65f259fb5
LeoDiazL <leo@bitovi.com>
2025-12-09 13:34:50 -0300

    Fix SSL policy issue

--- operations/deployment/terraform/modules/aws/lb/aws_lb.tf
+++ operations/deployment/terraform/modules/aws/lb/aws_lb.tf
@@ -92,8 +92,20 @@ resource "aws_lb_listener" "vm_alb_listener" {
     target_group_arn = aws_lb_target_group.vm_alb_tg[count.index].arn
   }
   # https://docs.aws.amazon.com/elasticloadbalancing/latest/application/create-https-listener.html
-  ssl_policy      = local.alb_listen_protocol[count.index] == "HTTPS" ? var.aws_alb_ssl_policy : null
-  certificate_arn = local.alb_listen_protocol[count.index] == "HTTPS" ? var.aws_certificates_selected_arn : null
+
+  dynamic "ssl_policy" {
+    for_each = local.alb_listen_protocol[count.index] == "HTTPS" ? [1] : []
+    content {
+      ssl_policy = var.aws_alb_ssl_policy
+    }
+  }
+
+  dynamic "certificate_arn" {
+    for_each = local.alb_listen_protocol[count.index] == "HTTPS" ? [1] : []
+    content {
+      certificate_arn = var.aws_certificates_selected_arn
+    }
+  }
 }
 
 # Attach EC2 instance(s) to target group(s)
