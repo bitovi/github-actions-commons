e3cf8beb6bd65db75079f32a3c925eafbfebd6f6
LeoDiazL <leo@bitovi.com>
2025-12-15 17:39:45 -0300

    More grouping

--- operations/deployment/terraform/modules/aws/lb/aws_lb.tf
+++ operations/deployment/terraform/modules/aws/lb/aws_lb.tf
@@ -148,6 +148,9 @@ resource "aws_alb_listener" "lb_listener" {
   }
 }
 
+locals {
+  http_listener_action = var.aws_certificate_enabled ? "redirect" : var.aws_alb_www_to_apex_redirect ? "fixed-response" : "forward"
+}
 
 resource "aws_alb_listener" "http_redirect" {
   count             = var.aws_alb_redirect_enable && !contains(local.alb_listen_port, 80) ? 1 : 0
@@ -155,36 +158,27 @@ resource "aws_alb_listener" "http_redirect" {
   port              = "80"
   protocol          = "HTTP"
 
-  dynamic "default_action" {
-    for_each = var.aws_certificate_enabled ? [1] : []
-    content {
-      type = "redirect"
-      redirect {
+  default_action {
+    type = local.http_listener_action
+
+    dynamic "redirect" {
+      for_each = local.http_listener_action == "redirect" ? [1] : []
+      content {
         port        = "443"
         protocol    = "HTTPS"
         status_code = "HTTP_301"
       }
     }
-  }
 
-  dynamic "default_action" {
-    for_each = !var.aws_certificate_enabled && var.aws_alb_www_to_apex_redirect ? [1] : []
-    content {
-      type = "fixed-response"
-      fixed_response {
+    dynamic "fixed_response" {
+      for_each = local.http_listener_action == "fixed-response" ? [1] : []
+      content {
         content_type = "text/plain"
         message_body = "Not Found"
         status_code  = "404"
       }
     }
-  }
-
-  dynamic "default_action" {
-    for_each = !var.aws_certificate_enabled && !var.aws_alb_www_to_apex_redirect ? [1] : []
-    content {
-      type             = "forward"
-      target_group_arn = aws_lb_target_group.vm_alb_tg[0].arn
-    }
+    target_group_arn = local.http_listener_action == "forward" ? aws_lb_target_group.vm_alb_tg[0].arn : null
   }
 
   depends_on = [
@@ -193,10 +187,54 @@ resource "aws_alb_listener" "http_redirect" {
   ]
 }
 
+#resource "aws_alb_listener" "http_redirect" {
+#  count             = var.aws_alb_redirect_enable && !contains(local.alb_listen_port, 80) ? 1 : 0
+#  load_balancer_arn = aws_lb.vm_alb.arn
+#  port              = "80"
+#  protocol          = "HTTP"
+#
+#  dynamic "default_action" {
+#    for_each = var.aws_certificate_enabled ? [1] : []
+#    content {
+#      type = "redirect"
+#      redirect {
+#        port        = "443"
+#        protocol    = "HTTPS"
+#        status_code = "HTTP_301"
+#      }
+#    }
+#  }
+#
+#  dynamic "default_action" {
+#    for_each = !var.aws_certificate_enabled && var.aws_alb_www_to_apex_redirect ? [1] : []
+#    content {
+#      type = "fixed-response"
+#      fixed_response {
+#        content_type = "text/plain"
+#        message_body = "Not Found"
+#        status_code  = "404"
+#      }
+#    }
+#  }
+#
+#  dynamic "default_action" {
+#    for_each = !var.aws_certificate_enabled && !var.aws_alb_www_to_apex_redirect ? [1] : []
+#    content {
+#      type             = "forward"
+#      target_group_arn = aws_lb_target_group.vm_alb_tg[0].arn
+#    }
+#  }
+#
+#  depends_on = [
+#    aws_lb.vm_alb,
+#    aws_lb_target_group.vm_alb_tg,
+#  ]
+#}
+
 resource "aws_lb_listener_rule" "http_forward_apex" {
   #count        = var.aws_alb_www_to_apex_redirect && var.aws_r53_domain_name != "" && !var.aws_certificate_enabled && length(aws_alb_listener.http_www_redirect) > 0 ? 1 : 0
   count        = var.aws_alb_www_to_apex_redirect && var.aws_r53_domain_name != "" && !var.aws_certificate_enabled && var.aws_alb_www_to_apex_redirect ? 1 : 0 #&& length(aws_alb_listener.http_www_redirect) > 0 ? 1 : 0
-  listener_arn = aws_alb_listener.http_redirect[0].arn #aws_alb_listener.http_www_redirect[0].arn
+  listener_arn = aws_alb_listener.http_redirect[0].arn                                                                                                         #aws_alb_listener.http_www_redirect[0].arn
   priority     = 20
 
   condition {
@@ -212,11 +250,11 @@ resource "aws_lb_listener_rule" "http_forward_apex" {
 }
 
 resource "aws_lb_listener_rule" "redirect_www_to_apex" {
- #count        = var.aws_alb_www_to_apex_redirect && var.aws_r53_domain_name != "" && (var.aws_certificate_enabled ? length(aws_alb_listener.https_redirect) > 0 : length(aws_alb_listener.http_www_redirect) > 0) ? 1 : 0
-  count        = var.aws_alb_www_to_apex_redirect && var.aws_r53_domain_name != "" && (var.aws_certificate_enabled ? length(aws_alb_listener.https_redirect) > 0 : var.aws_alb_www_to_apex_redirect ) ? 1 : 0  # # length(aws_alb_listener.http_www_redirect) > 0) ? 1 : 0
-  listener_arn = aws_alb_listener.http_redirect[0].arn 
+  #count        = var.aws_alb_www_to_apex_redirect && var.aws_r53_domain_name != "" && (var.aws_certificate_enabled ? length(aws_alb_listener.https_redirect) > 0 : length(aws_alb_listener.http_www_redirect) > 0) ? 1 : 0
+  count        = var.aws_alb_www_to_apex_redirect && var.aws_r53_domain_name != "" && (var.aws_certificate_enabled ? length(aws_alb_listener.https_redirect) > 0 : var.aws_alb_www_to_apex_redirect) ? 1 : 0 # # length(aws_alb_listener.http_www_redirect) > 0) ? 1 : 0
+  listener_arn = aws_alb_listener.http_redirect[0].arn
   #listener_arn = #var.aws_certificate_enabled ? aws_alb_listener.https_redirect[0].arn : aws_alb_listener.http_redirector[0].arn ##aws_alb_listener.http_www_redirect[0].arn
-  priority     = 10
+  priority = 10
 
   condition {
     host_header {
