fc41589f665679ad117ade0a17a7e5e255651f3b
LeoDiazL <leo@bitovi.com>
2026-01-16 11:00:29 -0300

    Comment out block

--- operations/deployment/terraform/modules/aws/eks/aws_eks_cluster.tf
+++ operations/deployment/terraform/modules/aws/eks/aws_eks_cluster.tf
@@ -173,27 +173,27 @@ locals {
 }
 
 
-resource "terraform_data" "replacement" {
-  input = yamlencode(distinct(concat(local.cluster_admin_roles, local.map_worker_roles)))
-}
-
-resource "kubernetes_config_map" "aws_auth" {
-  metadata {
-    name      = "aws-auth"
-    namespace = "kube-system"
-  }
-
-  data = {
-    mapRoles = yamlencode(distinct(concat(local.cluster_admin_roles, local.map_worker_roles)))
-    #mapUsers    = replace(yamlencode(var.map_additional_iam_users), "\"", local.yaml_quote)
-    mapAccounts = "${data.aws_caller_identity.current.account_id}"
-  }
-
-  lifecycle {
-    #replace_triggered_by = [terraform_data.replacement]
-    ignore_changes       = [all]
-  }
-}
+#resource "terraform_data" "replacement" {
+#  input = yamlencode(distinct(concat(local.cluster_admin_roles, local.map_worker_roles)))
+#}
+#
+#resource "kubernetes_config_map" "aws_auth" {
+#  metadata {
+#    name      = "aws-auth"
+#    namespace = "kube-system"
+#  }
+#
+#  data = {
+#    mapRoles = yamlencode(distinct(concat(local.cluster_admin_roles, local.map_worker_roles)))
+#    #mapUsers    = replace(yamlencode(var.map_additional_iam_users), "\"", local.yaml_quote)
+#    mapAccounts = "${data.aws_caller_identity.current.account_id}"
+#  }
+#
+#  lifecycle {
+#    #replace_triggered_by = [terraform_data.replacement]
+#    ignore_changes       = [*]
+#  }
+#}
 
 output "eks_kubernetes_provider_config" {
   value = {
