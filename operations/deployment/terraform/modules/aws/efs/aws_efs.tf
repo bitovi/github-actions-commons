3f052286578c2ad0537299626790eb364395d66f
LeoDiazL <leo@bitovi.com>
2026-01-26 13:07:40 -0300

    Test adding lifecycle to mount_targets

--- operations/deployment/terraform/modules/aws/efs/aws_efs.tf
+++ operations/deployment/terraform/modules/aws/efs/aws_efs.tf
@@ -40,12 +40,20 @@ data "aws_efs_file_system" "efs" {
   file_system_id = var.aws_efs_create ? aws_efs_file_system.efs[0].id : var.aws_efs_fs_id
 }
 
+resource "terraform_data" "efs_ha_tracker" {
+  input = var.aws_efs_create_ha
+}
+
 resource "aws_efs_mount_target" "efs_mount_target" {
   count           = var.aws_efs_create_mount_target ? length(local.aws_efs_subnets) : 0
   file_system_id  = var.aws_efs_create ? aws_efs_file_system.efs[0].id : var.aws_efs_fs_id
   subnet_id       = local.aws_efs_subnets[count.index]
   security_groups = [aws_security_group.efs_security_group[0].id]
   depends_on      = [aws_efs_file_system.efs]
+
+  lifecycle {
+    replace_triggered_by = [terraform_data.efs_ha_tracker]
+  }
 }
 
 resource "aws_efs_replication_configuration" "efs_rep_config" {
