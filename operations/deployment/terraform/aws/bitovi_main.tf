f2f39f895c66ca5090a36dd768cecb50c82c286b
LeoDiazL <leo@bitovi.com>
2026-01-26 10:21:50 -0300

    Change sg order

--- operations/deployment/terraform/aws/bitovi_main.tf
+++ operations/deployment/terraform/aws/bitovi_main.tf
@@ -627,8 +627,8 @@ module "efs_to_ecs_sg" {
   sg_rule_from_port        = 2049
   sg_rule_to_port          = 2049
   sg_rule_protocol         = "tcp"
-  source_security_group_id = module.efs[0].aws_efs_sg_id
-  target_security_group_id = module.aws_ecs[0].ecs_sg_id
+  source_security_group_id = module.aws_ecs[0].ecs_sg_id #module.efs[0].aws_efs_sg_id
+  target_security_group_id = module.efs[0].aws_efs_sg_id #module.aws_ecs[0].ecs_sg_id
   depends_on               = [module.aws_ecs, module.efs]
 }
 
